cd8ac6e909e58393a434d7e7dde5bbb3a4a3cf28
akaClay <domakaclay@gmail.com>
2022-11-05 02:18:38 -0700

    implement quest_tracker and aw_achieves

--- client/components/FeatureList/FeatureList.tsx
+++ client/components/FeatureList/FeatureList.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { updateFeature } from '../../store/slices/featureSlice';
+
+import type { SelectedFeature } from '../..//store/types';
+
 import './FeatureList.css';
 
 const FeatureList = () => {
+  const dispatch = useAppDispatch();
+  const feature = useAppSelector(state => state.feature.selected);
+  const featActive = (feat: SelectedFeature) => feat === feature ? 'active' : '';
+  const feats = {
+    quest_tracker: featActive('quest_tracker'),
+    aw_achieves: featActive('aw_achieves')
+  }
+
+  const switchFeat = (feat: SelectedFeature): void => {
+    dispatch(updateFeature(feat));
+  }
+
   return (
     <div className='feature-list'>
-      <div>
+      <div 
+        className={`feature ${feats.quest_tracker}`} 
+        onClick={() => switchFeat('quest_tracker')}
+      >
         Quest Tracker
       </div>
-      <div>
+      <div 
+        className={`feature ${feats.aw_achieves}`} 
+        onClick={() => switchFeat('aw_achieves')}
+      >
         Account-wide Achievements
       </div>
     </div>

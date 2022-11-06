5f3a19e8ffca1a74f2a9cce465276e0b628ad2bd
akaClay <domakaclay@gmail.com>
2022-11-06 01:52:45 -0800

    change quest_tracker to quests

--- api/spp_extras/spp_extras_api/urls.py
+++ api/spp_extras/spp_extras_api/urls.py
@@ -2,11 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views.characters import CharactersViewSet
-from .views.quest_tracker import QuestTrackerViewSet
+from .views.quests import QuestViewSet
 
 router = routers.DefaultRouter()
 router.register( 'characters', CharactersViewSet, basename='/characters/<string = expansion>' )
-router.register( 'quests', QuestTrackerViewSet, basename='/quests' )
+router.register( 'quests', QuestViewSet, basename='/quests' )
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- api/spp_extras/spp_extras_api/views/quests.py
+++ api/spp_extras/spp_extras_api/views/quests.py
@@ -1,10 +1,15 @@
+# Rest Framework
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+# Models
+from spp_extras_api.models.classiccharacters import CharacterQueststatus
+from spp_extras_api.models.classiccharacters import CharacterQueststatusWeekly
 
-class QuestTrackerViewSet(viewsets.ViewSet):
+
+class QuestViewSet(viewsets.ViewSet):
     @action(methods=['GET'], detail=False, name='Get Value from input')
     def all(self, request):
         input = request.GET.get('input')

395b3c80116db76065b9ef886b5a675991ddcb4d
akaClay <domakaclay@gmail.com>
2022-11-09 02:10:38 -0800

    implement allCompletedQuests

--- client/components/QuestTracker/QuestTrackerControls.tsx
+++ client/components/QuestTracker/QuestTrackerControls.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { updateCharacters } from '../../store/slices/characterSlice';
-import { updateCompletedQuests } from '../../store/slices/questSlice';
+import { updateCompletedQuests } from '../../store/slices/completedQuestSlice';
+import { updateAllQuests } from '../../store/slices/allQuestSlice';
+
 import { getCharacters } from '../../apiCalls/characters';
-import { getQuests } from '../../apiCalls/quests';
+import { getCompletedQuests, getAllQuests } from '../../apiCalls/quests';
+
 import type { SelectedExpansion, Character, CompletedQuests } from "../../store/types";
 
 import { faction } from '../../helpers/characters';
@@ -17,6 +20,7 @@ const QuestTrackerControls = () => {
   const expansion = useAppSelector(state => state.expansion.selected);
   const characters = useAppSelector(state => state.characters);
   const completedQuests = useAppSelector(state => state.completedQuests);
+  const allQuests = useAppSelector(state => state.allQuests);
   
   const storeCharacters = async ()  => {
     const chars = await getCharacters(expansion);
@@ -24,16 +28,27 @@ const QuestTrackerControls = () => {
     return chars;
   }
 
+  const storeAllQuests = async () => {
+    const quests = await getAllQuests(expansion);
+    dispatch(updateAllQuests(quests));
+  }
+
   const storeQuests = async () => {
-    const chars = await storeCharacters();
+    const allianceCharsExist = Object.keys(characters.alliance).length;
+    const hordeCharsExist = Object.keys(characters.horde).length;
+    const charsExist = allianceCharsExist || hordeCharsExist;
+    const chars = !charsExist ? await storeCharacters() : characters;
+
+    const allQuestsExist = Object.keys(allQuests.alliance).length;
+    !allQuestsExist ? await storeAllQuests() : allQuests;
+
     const allianceChars = Object.values(chars.alliance);
-    const allianceQuery = allianceChars.map((c: Character) => [c.guid, faction(c.race)]);
+    const allianceParams = allianceChars.map((c: Character) => [c.guid, faction(c.race)]);
     const hordeChars = Object.values(chars.horde);
-    const hordeQuery = hordeChars.map((c: Character) => [c.guid, faction(c.race)]);
-    const charQuery = allianceQuery.concat(hordeQuery).flat().join(',');
-    const completed = await getQuests(expansion, charQuery);
-    dispatch(updateCompletedQuests(completed));
-    console.log('COMPLETED QUESTS: ', completed)
+    const hordeParams = hordeChars.map((c: Character) => [c.guid, faction(c.race)]);
+    const charParams = allianceParams.concat(hordeParams).flat().join(',');
+    const allCompletedQuests = await getCompletedQuests(expansion, charParams);
+    dispatch(updateCompletedQuests(allCompletedQuests));
   }
 
   return (
--- client/components/QuestTracker/QuestTrackerView.tsx
+++ client/components/QuestTracker/QuestTrackerView.tsx
@@ -5,15 +5,14 @@ import { useAppSelector } from '../../store/hooks';
 const QuestTrackerView = () => {
   const characters = useAppSelector(state => state.characters);
   const completedQuests = useAppSelector(state => state.completedQuests);
+  const allQuests = useAppSelector(state => state.allQuests);
 
   console.log('ASDF ', completedQuests)
 
   return (
     <div>
       QT VIEW
-      {completedQuests.alliance ? Object.values(completedQuests.alliance).map(c => Object.values(c.reg).map(q => {
-        return <div>{q.guid}</div>
-      })) : <></>}
+      {allQuests.alliance ? Object.values(allQuests.alliance).map(c => <div>{c.entry}</div>) : <></>}
     </div>
   );
 }

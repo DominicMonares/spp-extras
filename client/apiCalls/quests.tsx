517c795715c936b0dececc8e9746faf223b1d3b3
akaClay <domakaclay@gmail.com>
2022-11-09 01:52:22 -0800

    change all quests url

--- client/apiCalls/quests.tsx
+++ client/apiCalls/quests.tsx
@@ -3,12 +3,22 @@ import { url } from '../config';
 import type { SelectedExpansion } from "../store/types";
 
 
-export const getQuests = async (expansion: SelectedExpansion, charQuery: string) => {
+export const getCompletedQuests = async (expansion: SelectedExpansion, charQuery: string) => {
   const expansionParams = new URLSearchParams(
     { expansion: expansion, chars: charQuery }
   );
 
   return fetch(`${url}/quests/completed?` + expansionParams)
+    .then(data => data.json())
+    .catch(err => console.log('API ERROR: ', err));
+}
+
+export const getAllQuests = async (expansion: SelectedExpansion) => {
+  const expansionParams = new URLSearchParams(
+    { expansion: expansion }
+  );
+
+  return fetch(`${url}/quests/all?` + expansionParams)
     .then(data => data.json())
     .catch(err => console.log('API ERROR: ', err));
 }

c2d01f02ebb313483f184e3bb2e8686b4f0e5390
akaClay <domakaclay@gmail.com>
2022-11-14 05:37:49 -0800

    save progress

--- client/components/QuestTracker/View.tsx
+++ client/components/QuestTracker/View.tsx
@@ -5,9 +5,11 @@ import { useAppSelector } from '../../store/hooks';
 const QuestTrackerView = () => {
   // const completedQuests = useAppSelector(state => state.completedQuests);
   const allQuests = useAppSelector(state => state.allQuests);
-  // const settings = useAppSelector(state => state.questTracker);
-  // const faction = settings.faction;
-  // const zone = settings.zone;
+  const settings = useAppSelector(state => state.questTracker);
+  const faction = settings.faction;
+  const zone = settings.zone;
+
+  // useEffect to fetch and sort by faction, zone, char, etc.
 
   return (
     <div>

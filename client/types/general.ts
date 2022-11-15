6acfb443078ec096d767273fc13222ff3bf94660
akaClay <domakaclay@gmail.com>
2022-11-15 00:20:13 -0800

    implement zone types

--- client/types/general.ts
+++ client/types/general.ts
@@ -4,6 +4,17 @@ export interface Expansion {
   selected: SelectedExpansion
 }
 
+export interface Subzone {
+  subzoneId: number,
+  subzone: string
+}
+
+export type Zone = Subzone[];
+
+export interface Zones {
+  [key: string]: Zone
+}
+
 export type SelectedFeature = 'quest_tracker' | 'aw_achieves' | null;
 
 export interface Feature {

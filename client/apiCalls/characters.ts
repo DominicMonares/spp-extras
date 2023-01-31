6dc66b76bdc60ec43eee383564d202e10d342c49
akaClay <domakaclay@gmail.com>
2023-01-30 22:07:19 -0800

    update urls

--- client/apiCalls/characters.ts
+++ client/apiCalls/characters.ts
@@ -1,11 +1,11 @@
 import type { FetchChars } from '../types';
-import { port, url } from '../config';
+import { port, httpUrl } from '../config';
 
 
 export const fetchCharacters: FetchChars = async expansion => {
   const expansionParams = new URLSearchParams({ expansion });
 
-  return fetch(`${url}:${port}/characters/all/?` + expansionParams)
+  return fetch(`${httpUrl}:${port}/characters/all/?` + expansionParams)
     .then(data => data.json())
     .catch(err => console.error('API ERROR: ', err)); // TEMP ERR HANDLING
 }

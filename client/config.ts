c5fc358d93b7055e6c3a6bcf496e2dc6763605dd
akaClay <domakaclay@gmail.com>
2023-01-30 22:09:31 -0800

    add httpUrl and wsUrl

--- client/config.ts
+++ client/config.ts
@@ -1,2 +1,3 @@
 export const port = process.env.NODE_ENV === 'development' ? 8000 : 80;
-export const url = 'http://127.0.0.1';
+export const httpUrl = 'http://127.0.0.1';
+export const wsUrl = 'ws://127.0.0.1';

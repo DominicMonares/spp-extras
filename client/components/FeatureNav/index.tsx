4c3d4a8079f59808cfa10cdc1620547d498a13ac
akaClay <domakaclay@gmail.com>
2023-01-31 19:43:39 -0800

    cleanup

--- client/components/FeatureNav/index.tsx
+++ client/components/FeatureNav/index.tsx
@@ -1,15 +1,12 @@
 import Features from './Features';
-import { useAppSelector } from '../../store/hooks';
 import './FeatureNav.css';
 
 
 const FeatureList = () => {
-  const expansion = useAppSelector(state => state.expansion.selected);
-
   return (
     <div className="feature-list">
       Features:
-      {expansion ? <Features /> : <></>}
+      <Features />
     </div>
   );
 }

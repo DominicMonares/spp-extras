329c7ca4ada9cc54b29ac6594c53eee884db7536
akaClay <domakaclay@gmail.com>
2023-02-09 01:57:12 -0800

    refactor models

--- api/src/spp_extras_api/views/characters.py
+++ api/src/spp_extras_api/views/characters.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from spp_extras_api.models.classicrealmd import Account
-from spp_extras_api.models.classiccharacters import Characters
+from spp_extras_api.models.classicrealmd import ClassicAccount
+from spp_extras_api.models.classiccharacters import ClassicCharacters
+from spp_extras_api.models.tbcrealmd import TbcAccount
+from spp_extras_api.models.tbccharacters import TbcCharacters
+from spp_extras_api.models.wotlkrealmd import WotlkAccount
+from spp_extras_api.models.wotlkcharacters import WotlkCharacters
 from spp_extras_api.utils.characters import get_account_ids, all_characters
 
 
@@ -10,15 +14,27 @@ class CharactersViewSet(viewsets.ViewSet):
     @action(methods=['GET'], detail=False)
     def all(self, request):
         expansion = request.GET.get('expansion')
+        accountModel = {}
+        charactersModel = {}
 
-        accounts = Account.objects\
+        if expansion == 'classic':
+            accountModel = ClassicAccount
+            charactersModel = ClassicCharacters
+        elif expansion == 'tbc': 
+            accountModel = TbcAccount
+            charactersModel = TbcCharacters
+        elif expansion == 'wotlk':
+            accountModel = WotlkAccount
+            charactersModel = WotlkCharacters
+
+        accounts = accountModel.objects\
             .using(f'{expansion}realmd')\
             .exclude(username__contains='RNDBOT')\
             .values('id', 'username')
 
         account_ids = list(map(get_account_ids, accounts))
         
-        characters = Characters.objects\
+        characters = charactersModel.objects\
             .using(f'{expansion}characters')\
             .filter(account__in=account_ids)\
             .values('guid', 'account', 'name', 'race', 'class_field')
--- api/src/spp_extras_api/views/quests.py
+++ api/src/spp_extras_api/views/quests.py
@@ -1,9 +1,22 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from spp_extras_api.models.classiccharacters import CharacterQueststatus, CharacterQueststatusWeekly
-from spp_extras_api.models.classicmangos import QuestTemplate
-from spp_extras_api.models.tbccharacters import CharacterQueststatusDaily, CharacterQueststatusMonthly
+from spp_extras_api.models.classiccharacters import\
+    ClassicCharacterQueststatus,\
+    ClassicCharacterQueststatusWeekly
+from spp_extras_api.models.classicmangos import ClassicQuestTemplate
+from spp_extras_api.models.tbccharacters import\
+    TbcCharacterQueststatus,\
+    TbcCharacterQueststatusDaily,\
+    TbcCharacterQueststatusWeekly,\
+    TbcCharacterQueststatusMonthly
+from spp_extras_api.models.tbcmangos import TbcQuestTemplate
+from spp_extras_api.models.wotlkcharacters import\
+    WotlkCharacterQueststatus,\
+    WotlkCharacterQueststatusDaily,\
+    WotlkCharacterQueststatusWeekly,\
+    WotlkCharacterQueststatusMonthly
+from spp_extras_api.models.wotlkmangos import WotlkQuestTemplate
 from spp_extras_api.utils.quests import all_completed_quests, all_quests
 
 
@@ -13,8 +26,26 @@ class QuestViewSet(viewsets.ViewSet):
     def completed(self, request):
         expansion = request.GET.get('expansion')
         characters = request.GET.get('characters').split(',')
-        chars = {}
+        regularQuestModel = {}
+        dailyQuestModel = {}
+        weeklyQuestModel = {}
+        monthlyQuestModel = {}
 
+        if expansion == 'classic':
+            regularQuestModel = ClassicCharacterQueststatus
+            weeklyQuestModel = ClassicCharacterQueststatusWeekly
+        elif expansion == 'tbc':
+            regularQuestModel = TbcCharacterQueststatus
+            dailyQuestModel = TbcCharacterQueststatusDaily
+            weeklyQuestModel = TbcCharacterQueststatusWeekly
+            monthlyQuestModel = TbcCharacterQueststatusMonthly
+        elif expansion == 'wotlk':
+            regularQuestModel = WotlkCharacterQueststatus
+            dailyQuestModel = WotlkCharacterQueststatusDaily
+            weeklyQuestModel = WotlkCharacterQueststatusWeekly
+            monthlyQuestModel = WotlkCharacterQueststatusMonthly
+
+        chars = {}
         for i, c in enumerate(characters):
             if i % 2 == 1:
                 continue
@@ -22,29 +53,29 @@ class QuestViewSet(viewsets.ViewSet):
 
         charIds = chars.keys()
 
-        completedRegular = CharacterQueststatus.objects\
+        completedRegular = regularQuestModel.objects\
             .using(f'{expansion}characters')\
             .filter(guid__in=charIds, status=1)\
             .values()
 
-        completedDaily = 0
-        # if expansion == 'tbc' or expansion =='wotlk':
-        #     completedDaily = CharacterQueststatusDaily.objects\
-        #         .using(f'{expansion}characters')\
-        #         .filter(guid__in=charIds)\
-        #         .values()
+        completedDaily = []
+        if expansion == 'tbc' or expansion =='wotlk':
+            completedDaily = dailyQuestModel.objects\
+                .using(f'{expansion}characters')\
+                .filter(guid__in=charIds)\
+                .values()
 
-        completedWeekly = CharacterQueststatusWeekly.objects\
+        completedWeekly = weeklyQuestModel.objects\
             .using(f'{expansion}characters')\
             .filter(guid__in=charIds)\
             .values()
 
-        completedMonthly = 0
-        # if expansion == 'tbc' or expansion == 'wotlk':
-        #     completedMonthly = CharacterQueststatusMonthly.objects\
-        #         .using(f'{expansion}characters')\
-        #         .filter(guid__in=charIds)\
-        #         .values()
+        completedMonthly = []
+        if expansion == 'tbc' or expansion == 'wotlk':
+            completedMonthly = monthlyQuestModel.objects\
+                .using(f'{expansion}characters')\
+                .filter(guid__in=charIds)\
+                .values()
 
         all_completed = all_completed_quests(
             chars,
@@ -63,8 +94,16 @@ class QuestViewSet(viewsets.ViewSet):
     # Get all quests from world database
     def all(self, request):
         expansion = request.GET.get('expansion')
+        questTemplateModel = {}
 
-        quests = QuestTemplate.objects\
+        if expansion == 'classic':
+            questTemplateModel = ClassicQuestTemplate
+        elif expansion == 'tbc':
+            questTemplateModel = TbcQuestTemplate
+        elif expansion == 'wotlk':
+            questTemplateModel = WotlkQuestTemplate
+
+        quests = questTemplateModel.objects\
             .using(f'{expansion}mangos')\
             .all()\
             .values(

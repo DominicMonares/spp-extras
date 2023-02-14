a77ada0b2ed11956242aabdaf56360c45d97e555
akaClay <domakaclay@gmail.com>
2023-02-14 03:09:13 -0800

    add sample data and additional character tests

--- tests/characterUtils.test.tsx
+++ tests/characterUtils.test.tsx
@@ -1,9 +1,46 @@
 import {
-  getFaction
+  getClass,
+  getFaction,
+  getRace
 } from '../client/utils';
+import {
+  dwarf,
+  dwarfCharacter,
+  orc,
+  orcCharacter,
+  paladin,
+  paladinCharacter,
+  shaman,
+  shamanCharacter
+} from './samples';
+
+
+describe('getClass', () => {
+  it('Should return a paladin class setting for a paladin character', () => {
+    expect(getClass(paladinCharacter)).toStrictEqual(paladin);
+  });
+
+  it('Should return a shaman class setting for a shaman character', () => {
+    expect(getClass(shamanCharacter)).toStrictEqual(shaman);
+  });
+});
 
 describe('getFaction', () => {
   it('Should return alliance for human race id', () => {
     expect(getFaction(1)).toBe('alliance');
+  });
+
+  it('Should return horde for blood elf race id', () => {
+    expect(getFaction(10)).toBe('horde');
+  });
+});
+
+describe('getRace', () => {
+  it('Should return a dwarf race for an dwarf character', () => {
+    expect(getRace(dwarfCharacter, 'alliance')).toStrictEqual(dwarf);
+  });
+
+  it('Should return an orc race for an orc character', () => {
+    expect(getRace(orcCharacter, 'horde')).toStrictEqual(orc);
   });
 });
---
+++ tests/samples/characters.ts
@@ -0,0 +1,59 @@
+export const dwarf = {
+  'title': 'Dwarf',
+  'id': 3,
+  'value': 4
+}
+
+export const dwarfCharacter = {
+  id: 1135,
+  name: 'Tolrig',
+  value: `{
+    "characterClass": 1,
+    "race": 3
+  }`
+};
+
+export const orc = {
+  'title': 'Orc',
+  'id': 2,
+  'value': 2
+}
+
+export const orcCharacter = {
+  id: 9563,
+  name: 'Drak',
+  value: `{
+    "characterClass": 9,
+    "race": 2
+  }`
+};
+
+export const paladin = {
+  'title': 'Paladin',
+  'id': 2,
+  'value': 2
+}
+
+export const paladinCharacter = {
+  id: 2500,
+  name: 'Smith',
+  value: `{
+    "characterClass": 2,
+    "race": 1
+  }`
+};
+
+export const shaman = {
+  'title': 'Shaman',
+  'id': 7,
+  'value': 64
+}
+
+export const shamanCharacter = {
+  id: 79411,
+  name: 'Gazjin',
+  value: `{
+    "characterClass": 7,
+    "race": 8
+  }`
+};

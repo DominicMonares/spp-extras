5d331ad682707f9a7a396bad4687dad511c70514
akaClay <domakaclay@gmail.com>
2023-02-15 20:30:39 -0800

    fix structure

--- tests/samples/filteredTemplateQuests.ts
+++ tests/samples/filteredTemplateQuests.ts
@@ -1,26 +1,52 @@
 export const filteredHordeQuests = {
-  alliance: {},
-  horde: {
-    8: {
-      entry: 8,
-      requiredclasses: 0,
-      requiredraces: 178,
-      title: 'A Rogue\'s Deal',
-      type: 0,
-      zoneorsort: 154,
-      questflags: 0,
-      completed: true
-    },
-    25: {
-      entry: 25,
-      requiredclasses: 0,
-      requiredraces: 178,
-      title: 'Stonetalon Standstill',
-      type: 0,
-      zoneorsort: 331,
-      questflags: 12,
-      completed: false
-    }
+  8: {
+    entry: 8,
+    requiredclasses: 0,
+    requiredraces: 178,
+    title: 'A Rogue\'s Deal',
+    type: 0,
+    zoneorsort: 154,
+    questflags: 0,
+    completed: true
   },
-  both: {}
+  25: {
+    entry: 25,
+    requiredclasses: 0,
+    requiredraces: 178,
+    title: 'Stonetalon Standstill',
+    type: 0,
+    zoneorsort: 331,
+    questflags: 12,
+    completed: false
+  },
+  9884: {
+    entry: 9884,
+    requiredclasses: 0,
+    requiredraces: 0,
+    title: 'Membership Benefits',
+    type: 0,
+    zoneorsort: 3518,
+    questflags: 136,
+    completed: true
+  },
+  11389: {
+    entry: 11389,
+    requiredclasses: 0,
+    requiredraces: 0,
+    title: 'Wanted: A Warp Splinter Clipping',
+    type: 85,
+    zoneorsort: 3845,
+    questflags: 4232,
+    completed: true
+  },
+  24584: {
+    entry: 24584,
+    requiredclasses: 0,
+    requiredraces: 0,
+    title: 'Malygos Must Die!',
+    type: 62,
+    zoneorsort: 4500,
+    questflags: 32968,
+    completed: true
+  }
 };

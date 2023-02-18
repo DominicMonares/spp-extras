e53c12d78c9f2f8d4bcd2a3f4111c6fad936de19
akaClay <domakaclay@gmail.com>
2023-02-17 22:07:50 -0800

    add additional data to quest render

--- client/components/QuestTracker/Quest.tsx
+++ client/components/QuestTracker/Quest.tsx
@@ -5,7 +5,12 @@ const Quest = ({ quest }: QuestProps) => {
   return (
     <div>
       <span className={quest.completed ? 'complete' : 'incomplete'}>
-        {quest.title}
+        {`
+          ${quest.title} |
+          Quest: ${quest.entry} |
+          Race: ${quest.requiredraces} |
+          Class: ${quest.requiredclasses}
+        `}
       </span>
     </div>
   );

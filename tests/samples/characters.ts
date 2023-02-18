b715e912c77361a7c1abe35c4bf8340a9ddcd9dc
akaClay <domakaclay@gmail.com>
2023-02-18 00:12:11 -0800

    add newline under imports

--- tests/samples/characters.ts
+++ tests/samples/characters.ts
@@ -1,3 +1,6 @@
+import { Characters } from "../../client/types";
+
+
 export const bloodElf = {
   'title': 'Blood Elf',
   'id': 10,
@@ -63,3 +66,25 @@ export const shamanCharacter = {
     "race": 8
   }`
 };
+
+export const storedCharacters = {
+  alliance: {},
+  horde: {
+    2501: {
+      guid: 2501,
+      account: 1,
+      name: 'Gaz',
+      race: 8,
+      class_field: 7,
+      account_name: 'ACCOUNT1'
+    },
+    4501: {
+      guid: 4501,
+      account: 1,
+      name: 'Drak',
+      race: 2,
+      class_field: 3,
+      account_name: 'ACCOUNT1'
+    }
+  }
+} as Characters;

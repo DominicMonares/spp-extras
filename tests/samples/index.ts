a89ec8829440e1446dc5103c00dee9ba3b0283f5
akaClay <domakaclay@gmail.com>
2023-02-17 22:44:13 -0800

    remove questControlSettings

--- tests/samples/index.ts
+++ tests/samples/index.ts
@@ -1,6 +1,5 @@
 export * from './characterQuests';
 export * from './characters';
 export * from './filteredTemplateQuests';
-export * from './questControlSettings';
 export * from './questViewSettings';
 export * from './templateQuests';

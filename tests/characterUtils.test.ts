e267b40646b778b14537f7a26f6af4a4ff93b5c7
akaClay <domakaclay@gmail.com>
2023-02-18 01:39:56 -0800

    add newlines

--- tests/characterUtils.test.ts
+++ tests/characterUtils.test.ts
@@ -20,25 +20,30 @@ describe('getClass', () => {
     expect(getClass(paladinCharacter)).toStrictEqual(paladin);
   });
 
+
   it('Should return a shaman class setting for a shaman character', () => {
     expect(getClass(shamanCharacter)).toStrictEqual(shaman);
   });
 });
+
 
 describe('getFaction', () => {
   it('Should return alliance for human race id', () => {
     expect(getFaction(1)).toBe('alliance');
   });
 
+
   it('Should return horde for blood elf race id', () => {
     expect(getFaction(10)).toBe('horde');
   });
 });
 
+
 describe('getRace', () => {
   it('Should return a dwarf race for an dwarf character', () => {
     expect(getRace(dwarfCharacter, 'alliance')).toStrictEqual(dwarf);
   });
+
 
   it('Should return an orc race for an orc character', () => {
     expect(getRace(orcCharacter, 'horde')).toStrictEqual(orc);

d8b1296ac76bc9059015dd7d1130f59c5e3f1e10
akaClay <domakaclay@gmail.com>
2023-02-21 03:01:26 -0800

    remove db_connected

--- api/src/spp_extras_api/views/characters.py
+++ api/src/spp_extras_api/views/characters.py
@@ -8,19 +8,12 @@ from spp_extras_api.models.tbccharacters import TbcCharacters
 from spp_extras_api.models.wotlkrealmd import WotlkAccount
 from spp_extras_api.models.wotlkcharacters import WotlkCharacters
 from spp_extras_api.utils.characters import get_account_id, all_characters
-from spp_extras_api.utils.db_check import db_connected
 
 
 class CharactersViewSet(viewsets.ViewSet):
     @action(methods=['GET'], detail=False)
     def all(self, request):
         expansion = request.GET.get('expansion')
-        if (not db_connected(expansion)):
-            return Response(
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-                data='Unable to establish connection to database...'
-        )
-
         account_model = {}
         characters_model = {}
 
@@ -33,7 +26,7 @@ class CharactersViewSet(viewsets.ViewSet):
         elif expansion == 'wotlk':
             account_model = WotlkAccount
             characters_model = WotlkCharacters
-
+            
         # Fetch all player account data, excluding random bot accounts
         accounts = account_model.objects\
             .using(f'{expansion}realmd')\

83f115d7164f2877b67448e9b6d92861f3b57f72
akaClay <domakaclay@gmail.com>
2023-02-22 01:02:39 -0800

    refactor response handling

--- client/apiCalls/characters.ts
+++ client/apiCalls/characters.ts
@@ -5,10 +5,11 @@ import { FetchCharacters } from '../types';
 export const fetchCharacters: FetchCharacters = async expansion => {
   const expansionParams = new URLSearchParams({ expansion });
 
-  return fetch(`${httpUrl}:${port}/characters/all/?` + expansionParams)
-    .then(data => data.json())
-    .catch(err => { 
-      console.log('ERRR ', err)
-      throw err
-     });
+  return await fetch(`${httpUrl}:${port}/characters/all/?` + expansionParams)
+    .then(async data => {
+      const status = data.status;
+      const response = await data.json();
+      if (status === 200) return response;
+      else throw response;
+    });
 }

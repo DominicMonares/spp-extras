75f15a8e039c3387a330ac44b4452a9744f32fc1
akaClay <domakaclay@gmail.com>
2023-02-23 19:26:43 -0800

    implement error handling

--- client/apiCalls/quests.ts
+++ client/apiCalls/quests.ts
@@ -3,17 +3,23 @@ import { FetchCompQuests, FetchTemplateQuests } from '../types';
 
 
 export const fetchCompletedQuests: FetchCompQuests = async (expansion, characters) => {
-  const expansionParams = new URLSearchParams({ expansion, characters });
-
-  return fetch(`${httpUrl}:${port}/quests/completed/?` + expansionParams)
-    .then(data => data.json())
-    .catch(err => { throw err });
+  const parameters = new URLSearchParams({ expansion, characters });
+  return fetch(`${httpUrl}:${port}/quests/completed/?` + parameters)
+    .then(async data => {
+      const status = data.status;
+      const response = await data.json();
+      if (status === 200) return response;
+      else throw response;
+    });
 }
 
 export const fetchTemplateQuests: FetchTemplateQuests = async expansion => {
-  const expansionParams = new URLSearchParams({ expansion });
-
-  return fetch(`${httpUrl}:${port}/quests/all/?` + expansionParams)
-    .then(data => data.json())
-    .catch(err => { throw err });
+  const parameters = new URLSearchParams({ expansion });
+  return fetch(`${httpUrl}:${port}/quests/all/?` + parameters)
+    .then(async data => {
+      const status = data.status;
+      const response = await data.json();
+      if (status === 200) return response;
+      else throw response;
+    });
 }

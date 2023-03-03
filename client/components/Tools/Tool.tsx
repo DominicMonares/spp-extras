8680379be4b0660e76cee9fb9cda3f41717f6b48
akaClay <domakaclay@gmail.com>
2023-03-02 21:32:44 -0800

    save button hover progress

--- client/components/Tools/Tool.tsx
+++ client/components/Tools/Tool.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { storeTool } from '../../store/slices';
 import { ToolNavProps, SelectedTool } from '../../types';
@@ -9,6 +10,7 @@ import './Tools.css';
 const Tool = ({ tool, name }: ToolNavProps) => {
   const dispatch = useAppDispatch();
   const selectedTool = useAppSelector(state => state.feature.selected);
+  const [hovering, setHovering] = useState<string>('-hovering');
 
   const switchFeature = (feat: SelectedTool): void => {
     dispatch(storeTool(feat));
@@ -17,23 +19,25 @@ const Tool = ({ tool, name }: ToolNavProps) => {
   return (
     <li className={`tool ${tool === selectedTool ? 'active' : ''}`}>
       {tool === 'accountAchievements' ? (
-        <button
-          className="tool-button"
-          onClick={() => switchFeature(tool)}
-        >
+        <button className="tool-button">
           <img src={bigButton} />
           <div className="tool-big-text-container">
             <div className="tool-label-text">{name.split(' ')[0]}</div>
-            <div className="tool-label-text last-label-text">{name.split(' ')[1]}</div>
+            <div className="tool-label-text">{name.split(' ')[1]}</div>
+            <div className="tool-big-button-overlay" onClick={() => switchFeature(tool)}></div>
           </div>
         </button>
       ) : (
         <button className="tool-button">
-          <img className="tool-button-img" src={button} />
-          <div className="tool-text-container">
-            <div className="tool-label-text" onClick={() => switchFeature(tool)}>
-              {name}
-            </div>
+          <img className={`tool-button-img${hovering}`} src={button} />
+          <div className={`tool-text-container${hovering}`}>
+            <div className={`tool-label-text${hovering}`}>{name}</div>
+            <div 
+              className={`tool-button-overlay${hovering}`}
+              onClick={() => switchFeature(tool)}
+              onMouseEnter={() => setHovering('-hovering')}
+              onMouseLeave={() => setHovering('')}
+            />
           </div>
         </button>
       )}

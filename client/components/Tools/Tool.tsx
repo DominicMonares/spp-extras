c7691134d7e2aecf50349f462c32b98c3d60d1db
akaClay <domakaclay@gmail.com>
2023-03-09 16:11:44 -0800

    implement active class

--- client/components/Tools/Tool.tsx
+++ client/components/Tools/Tool.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { storeTool } from '../../store/slices';
 import { ToolNavProps, SelectedTool } from '../../types';
@@ -9,32 +9,37 @@ const Tool = ({ tool, name }: ToolNavProps) => {
   const dispatch = useAppDispatch();
   const selectedTool = useAppSelector(state => state.feature.selected);
   const [hovering, setHovering] = useState<string>('');
+  const [active, setActive] = useState<string>('');
+
+  useEffect(() => {
+    tool === selectedTool ? setActive('-active') : setActive('');
+  });
 
   const switchFeature = (feat: SelectedTool): void => {
+    setActive('-active');
     dispatch(storeTool(feat));
   }
 
   return (
-    <li className={`tool ${tool === selectedTool ? 'active' : ''}`}>
+    <li className="tool">
       {tool === 'accountAchievements' ? (
         <button
-          className={`tool-big-button${hovering}`}
+          className={`tool-big-button${hovering}${active}`}
           onClick={() => switchFeature(tool)}
           onMouseEnter={() => setHovering('-hovering')}
           onMouseLeave={() => setHovering('')}
         >
-          <div className={`tool-big-button-text${hovering}`}>{name.split(' ')[0]}</div>
-          <div className={`tool-big-button-text${hovering}`}>{name.split(' ')[1]}</div>
+          <div className={`tool-big-button-text${hovering}${active}`}>{name.split(' ')[0]}</div>
+          <div className={`tool-big-button-text${hovering}${active}`}>{name.split(' ')[1]}</div>
         </button>
-
       ) : (
         <button
-          className={`tool-button${hovering}`}
+          className={`tool-button${hovering}${active}`}
           onClick={() => switchFeature(tool)}
           onMouseEnter={() => setHovering('-hovering')}
           onMouseLeave={() => setHovering('')}
         >
-          <div className={`tool-button-text${hovering}`}>Quest Tracker</div>
+          <div className={`tool-button-text${hovering}${active}`}>Quest Tracker</div>
         </button>
       )}
     </li>

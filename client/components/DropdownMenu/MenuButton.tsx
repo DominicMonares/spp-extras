14a2dd3ba132da730b3292751e7610e6f283c6b2
akaClay <domakaclay@gmail.com>
2023-03-12 01:42:33 -0800

    cleanup

--- client/components/DropdownMenu/MenuButton.tsx
+++ client/components/DropdownMenu/MenuButton.tsx
@@ -9,15 +9,15 @@ const MenuButton = ({ dropdown, items, selected }: MenuButtonProps) => {
 
   return (
     <button
-      className={`menu-button${hovering}`}
+      className={`dd-main-button${hovering}`}
       type="button"
       aria-haspopup="menu"
       aria-expanded={dropdown ? 'true' : 'false'}
       onMouseEnter={() => setHovering('-hovering')}
       onMouseLeave={() => setHovering('')}
     >
-      <img className="arrow" src={arrow} />
-      <div className={`menu-button-text${hovering}`}>
+      <img className="dd-arrow" src={arrow} />
+      <div className={`dd-main-button-text${hovering}`}>
         {selected() ? selected() : items.title}
       </div>
     </button>

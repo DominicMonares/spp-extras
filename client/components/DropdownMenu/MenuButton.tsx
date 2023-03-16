283d776aff7bd6d12e5c6fbfe767610c1973b9ed
akaClay <domakaclay@gmail.com>
2023-03-16 02:39:25 -0700

    font size styling

--- client/components/DropdownMenu/MenuButton.tsx
+++ client/components/DropdownMenu/MenuButton.tsx
@@ -7,16 +7,10 @@ import './DropdownMenu.css';
 const MenuButton = ({ selected, subHovering, title }: MenuButtonProps) => {
   const [hovering, setHovering] = useState<string>('');
   useEffect(() => subHovering ? setHovering('-hovering') : setHovering(''));
+  
   const titleClass = () => {
     const titleLength = selected() ? selected().length : title.length;
-    const short = titleLength <= 9 ? '-short' : '';
-    const lowerMid = titleLength > 9 && titleLength <= 12 ? '-lower-mid' : '';
-    const mid = titleLength > 12 && titleLength <= 15 ? '-mid' : '';
-    const upperMid = titleLength > 15 && titleLength <= 17 ? '-upper-mid' : ''; 
-    const long = titleLength > 17 && titleLength <= 21 ? '-long' : '';
-    const extraLong = titleLength > 21 && titleLength <= 24 ? '-extra-long' : ''; 
-    const jumbo = titleLength > 24 ? '-jumbo' : '';
-    return `dd-button-text${short}${lowerMid}${mid}${upperMid}${long}${extraLong}${jumbo}`;
+    return titleLength <= 12 ? 12 : titleLength;
   }
 
   return (
@@ -26,7 +20,7 @@ const MenuButton = ({ selected, subHovering, title }: MenuButtonProps) => {
       onMouseLeave={() => setHovering('')}
     >
       <img className={`dd-main-arrow${hovering}`} src={arrow} />
-      <div className={`${titleClass()}${hovering}`}>
+      <div className={`c${titleClass()}${hovering}`}>
         {selected() ? selected() : title}
       </div>
     </button>

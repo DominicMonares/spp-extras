f7c5582a1bb70ea9187fbb6e8b46efb00d7f32f4
akaClay <domakaclay@gmail.com>
2023-03-17 21:24:08 -0700

    cleanup

--- client/components/Controls/QuestTrackerControls.tsx
+++ client/components/Controls/QuestTrackerControls.tsx
@@ -11,7 +11,7 @@ import {
   raceMenu,
   zoneMenu
 } from '../../utils';
-import label from '../../assets/labels/small-label.png';
+
 import './Controls.css';
 
 
@@ -35,19 +35,15 @@ const QuestTrackerControls = () => {
   }
 
   return (
-    <div className={`${expansion}-qt-controls`}>
-      <div className="controls-label">
-        <img src={label} />
-        <div className="controls-label-text">Controls</div>
-      </div>
+    <>
       <div className="qt-faction">
-        <div className="filter-label">Select Faction:</div>
+        <div className="control-label">Select Faction:</div>
         <FactionSelect />
       </div>
       <div className="qt-filters">
-        <div className="filter-label filter-zone">Select Zone:</div>
+        <div className="control-label filter-zone">Select Zone:</div>
         <DropdownMenu dropdownType="zone" menu={zoneMenu(expansion, zone)} />
-        <div className="filter-label filter-or">~ or ~</div>
+        <div className="control-label filter-or">~ or ~</div>
         {character && JSON.parse(character.value).characterClass ? (
           <button onClick={dispatchCharacterClass}>
             {getClass(character)?.title}
@@ -67,14 +63,14 @@ const QuestTrackerControls = () => {
         )}
       </div>
       <div className="qt-additional-filters">
-        <div className="filter-label filter-additional">Additional Filters:</div>
+        <div className="control-label filter-additional">Additional Filters:</div>
         <DropdownMenu 
           dropdownType="character" 
           menu={characterMenu(character, characters, faction)} 
         />
         <DropdownMenu dropdownType="type" menu={questTypeMenu(expansion, type)} />
       </div>
-    </div>
+    </>
   );
 }
 
--- client/components/Controls/index.tsx
+++ client/components/Controls/index.tsx
@@ -1,12 +1,18 @@
 import QuestTrackerControls from './QuestTrackerControls';
 import { useAppSelector } from '../../store/hooks';
+import label from '../../assets/labels/small-label.png';
 
 
 const Controls = () => {
+  const expansion = useAppSelector(state => state.expansion.selected);
   const tool = useAppSelector(state => state.tool.selected);
 
   return (
-    <div>
+    <div className={`controls ${expansion}-controls`}>
+      <div className="controls-header">
+        <img src={label} />
+        <div className="controls-header-text">Controls</div>
+      </div>
       {tool === 'questTracker' ? (
         <QuestTrackerControls />
       ) : (

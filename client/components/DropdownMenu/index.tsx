5532440b56ec4e0c47bee9162607e0a39ae32d7a
akaClay <domakaclay@gmail.com>
2023-03-21 16:40:45 -0700

    cleanup

--- client/components/DropdownMenu/index.tsx
+++ client/components/DropdownMenu/index.tsx
@@ -12,9 +12,13 @@ const DropdownMenu = ({ depthLevel, dropdown, dropdownType, menu }: DropdownProp
 
   return (
     <ul className={`
-        ${depthLevel ? `dropdown${size}${level}` : ''}
-        ${dropdown ? 'dropdown-show' : ''}
-      `}>
+      ${depthLevel ? (
+        `dropdown${size}${level} ${depthLevel === 3 ? 'dd-scroll' : ''}`
+      ) : (
+        ''
+      )}
+      ${dropdown ? 'dropdown-show' : ''}
+    `}>
       {menu.map((m: Submenu, i: number) => {
         return (
           <MenuItems

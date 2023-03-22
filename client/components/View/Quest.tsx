cc7bb136c472509e78bf9670c06da13a87d26926
akaClay <domakaclay@gmail.com>
2023-03-21 21:37:22 -0700

    transparency and scrollbar changes

--- client/components/View/Quest.tsx
+++ client/components/View/Quest.tsx
@@ -1,14 +1,18 @@
 import { useAppSelector } from '../../store/hooks';
 import { QuestProps } from '../../types';
+import complete from '../../assets/labels/complete.png';
+import incomplete from '../../assets/labels/incomplete.png';
 
 
 const Quest = ({ quest }: QuestProps) => {
-  const expansion = useAppSelector(state => state.expansion.selected);
-
   return (
     <tr>
-      <td className={`qt-td3 ${expansion}${quest.completed ? '-qtc' : '-qti'}`}>
-        {quest.completed ? 'Complete' : 'Incomplete'}
+      <td className="qt-td3">
+        {quest.completed ? (
+          <img className="qt-complete" src={complete} />
+        ) : (
+          <img className="qt-incomplete" src={incomplete} />
+        )}
       </td>
       <td className="qt-td2">{quest.entry}</td>
       <td className="qt-td1">{quest.title}</td>

c8f12e39e31181e9c9c0bbdee30ddb7b3790d4b8
akaClay <domakaclay@gmail.com>
2023-03-21 23:39:08 -0700

    implement modal

--- client/components/View/Quest.tsx
+++ client/components/View/Quest.tsx
@@ -1,14 +1,46 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { useAppSelector } from '../../store/hooks';
 import { QuestProps } from '../../types';
 import complete from '../../assets/labels/complete.png';
 import incomplete from '../../assets/labels/incomplete.png';
 
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root');
+
 const Quest = ({ quest }: QuestProps) => {
   const expansion = useAppSelector(state => state.expansion.selected);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
 
   return (
     <tr className={`${expansion}-quest`}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Wowhead"
+      >
+        <div onClick={closeModal}>WARNING</div>
+      </Modal>
       <td className="qt-td3">
         {quest.completed ? (
           <img className="qt-complete" src={complete} />
@@ -17,9 +49,15 @@ const Quest = ({ quest }: QuestProps) => {
         )}
       </td>
       <td className="qt-td2">{quest.entry}</td>
-      <td className="qt-td1">{quest.title}</td>
+      <td className="qt-td1">
+        <div className="qt-quest" onClick={openModal}>{quest.title}</div>
+      </td>
     </tr>
   );
 }
+
+{/* <a href={`https://www.wowhead.com/${expansion}/quest=${quest.entry}`} target="_blank">
+{quest.title}
+</a> */}
 
 export default Quest;

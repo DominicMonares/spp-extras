ed35c41dd4b1d9052788c18badb7ecee7fceab02
akaClay <domakaclay@gmail.com>
2023-03-22 17:58:33 -0700

    reorganize

--- client/types/questTracker.ts
+++ client/types/questTracker.ts
@@ -1,12 +1,19 @@
 import { 
   CharacterClass, 
+  Characters,
   Faction, 
   Race
 } from './characters';
+import { Menu } from './dropdown';
+import { SelectedExpansion } from './expansions';
 import {
   CharacterQuestClass,
   CharacterQuestRace,
-  QuestType
+  CharacterQuests,
+  CompletedQuests,
+  QuestType,
+  TemplateQuest,
+  TemplateQuests
 } from './quests';
 
 
@@ -20,6 +27,64 @@ export interface ClassSetting {
   id: CharacterClass;
   title: string;
   value: CharacterQuestClass;
+}
+
+export type CreateViewQuests = (
+  completedQuests: CompletedQuests | Record<string,never>,
+  settings: QuestTrackerSettings,
+  templateQuests: TemplateQuests | Record<string,never>
+) => ViewQuests;
+
+export type FilterQuests = (
+  settings: QuestTrackerSettings,
+  templateQuests: TemplateQuests | Record<string,never>
+) => ViewQuests;
+
+export type FilteredCharacterMenu = (
+  chararcter: CharacterSetting | Record<string,never> | undefined,
+  characters: Characters,
+  faction: Faction
+) => Menu;
+
+export type FilteredClassMenu = (
+  expansion: SelectedExpansion,
+  faction: Faction,
+  characterClass: ClassSetting | Record<string,never> | undefined
+) => Menu;
+
+export type FilteredQuestTypeMenu = (
+  expansion: SelectedExpansion,
+  type: QuestType | Record<string,never> | undefined
+) => Menu;
+
+export type FilteredRaceMenu = (
+  expansion: SelectedExpansion,
+  faction: Faction,
+  race: RaceSetting | Record<string,never> | undefined
+) => Menu;
+
+export type FilteredZoneMenu = (
+  expansion: SelectedExpansion,
+  zone: string | Record<string,never> | undefined
+) => Menu;
+
+export type MarkTemplateQuests = (
+  characterQuests: CharacterQuests,
+  filteredTemplateQuests: ViewQuests,
+  type: QuestType
+) => ViewQuests;
+
+export interface QuestCondition {
+  setting: Faction | QuestType | string | ClassSetting | RaceSetting | CharacterSetting | undefined | Record<string,never>;
+  conditionMet: () => boolean;
+}
+
+export interface QuestConditions {
+  [key: string]: QuestCondition;
+}
+
+export interface QuestProps {
+  quest: ViewQuest;
 }
 
 export interface QuestTrackerSettings {
@@ -36,3 +101,23 @@ export interface RaceSetting {
   title: string;
   value: CharacterQuestRace;
 }
+
+export interface ViewQuest extends TemplateQuest {
+  completed: boolean;
+}
+
+export interface ViewQuests {
+  [key: string]: ViewQuest;
+}
+
+export interface ViewSubzone {
+  subzoneId: number;
+  subzone: string;
+}
+
+export type ViewZone = ViewSubzone[];
+
+export interface ViewZones {
+  [key: string]: ViewZone;
+}
+
--- client/types/quests.ts
+++ client/types/quests.ts
@@ -1,12 +1,4 @@
-import { Characters, Faction } from './characters';
-import { Menu } from './dropdown';
-import { SelectedExpansion } from './expansions';
-import {
-  CharacterSetting,
-  ClassSetting,
-  QuestTrackerSettings,
-  RaceSetting
-} from './questTracker';
+import { Faction } from './characters';
 
 
 export interface CharacterQuests {
@@ -25,54 +17,9 @@ export interface CompletedQuests {
   horde: FactionQuests;
 }
 
-export type CreateViewQuests = (
-  completedQuests: CompletedQuests | Record<string,never>,
-  settings: QuestTrackerSettings,
-  templateQuests: TemplateQuests | Record<string,never>
-) => ViewQuests;
-
 export interface FactionQuests {
   [key: string]: CharacterQuests;
 }
-
-export type FilterQuests = (
-  settings: QuestTrackerSettings,
-  templateQuests: TemplateQuests | Record<string,never>
-) => ViewQuests;
-
-export type FilteredCharacterMenu = (
-  chararcter: CharacterSetting | Record<string,never> | undefined,
-  characters: Characters,
-  faction: Faction
-) => Menu;
-
-export type FilteredClassMenu = (
-  expansion: SelectedExpansion,
-  faction: Faction,
-  characterClass: ClassSetting | Record<string,never> | undefined
-) => Menu;
-
-export type FilteredQuestTypeMenu = (
-  expansion: SelectedExpansion,
-  type: QuestType | Record<string,never> | undefined
-) => Menu;
-
-export type FilteredRaceMenu = (
-  expansion: SelectedExpansion,
-  faction: Faction,
-  race: RaceSetting | Record<string,never> | undefined
-) => Menu;
-
-export type FilteredZoneMenu = (
-  expansion: SelectedExpansion,
-  zone: string | Record<string,never> | undefined
-) => Menu;
-
-export type MarkTemplateQuests = (
-  characterQuests: CharacterQuests,
-  filteredTemplateQuests: ViewQuests,
-  type: QuestType
-) => ViewQuests;
 
 export interface Quest {
   guid: number;
@@ -91,24 +38,11 @@ export interface Quest {
   itemcount4?: number;
 }
 
-export interface QuestCondition {
-  setting: Faction | QuestType | string | ClassSetting | RaceSetting | CharacterSetting | undefined | Record<string,never>;
-  conditionMet: () => boolean;
-}
-
-export interface QuestConditions {
-  [key: string]: QuestCondition;
-}
-
 export interface QuestFlags {
   regular: number[];
   daily?: number[];
   weekly: number[];
   monthly?: number[];
-}
-
-export interface QuestProps {
-  quest: ViewQuest;
 }
 
 export interface QuestRace {
@@ -153,21 +87,3 @@ export interface TemplateQuests {
   both: TemplateFactionQuests | Record<string,never>;
 }
 
-export interface ViewQuest extends TemplateQuest {
-  completed: boolean;
-}
-
-export interface ViewQuests {
-  [key: string]: ViewQuest;
-}
-
-export interface ViewSubzone {
-  subzoneId: number;
-  subzone: string;
-}
-
-export type ViewZone = ViewSubzone[];
-
-export interface ViewZones {
-  [key: string]: ViewZone;
-}

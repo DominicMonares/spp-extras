0cecd1a947d5e97c96f483b78ef02ea40e59a407
akaClay <domakaclay@gmail.com>
2023-03-22 20:38:09 -0700

    cleanup

--- client/components/Controls/QuestTrackerControls.tsx
+++ client/components/Controls/QuestTrackerControls.tsx
@@ -1,12 +1,9 @@
 import DropdownMenu from '../DropdownMenu';
 import FactionSelect from './FactionSelect';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { storeQuestTrackerClass, storeQuestTrackerRace } from '../../store/slices';
+import { useAppSelector } from '../../store/hooks';
 import {
   characterMenu,
   classMenu,
-  getClass,
-  getRace,
   questTypeMenu,
   raceMenu,
   zoneMenu
@@ -15,23 +12,10 @@ import './Controls.css';
 
 
 const QuestTrackerControls = () => {
-  const dispatch = useAppDispatch();
   const characters = useAppSelector(state => state.characters)
   const expansion = useAppSelector(state => state.expansion.selected);
   const settings = useAppSelector(state => state.questTracker);
   const { character, characterClass, faction, race, type, zone } = settings;
-
-  const dispatchCharacterClass = () => {
-    dispatch(storeQuestTrackerClass({
-      characterClass: getClass(character)
-    }));
-  }
-
-  const dispatchCharacterRace = () => {
-    dispatch(storeQuestTrackerRace({
-      race: getRace(character, faction)
-    }));
-  }
 
   return (
     <>
@@ -43,29 +27,17 @@ const QuestTrackerControls = () => {
         <div className="control-label qt-zone-label">Select Zone:</div>
         <DropdownMenu dropdownType="zone" menu={zoneMenu(expansion, zone)} />
         <div className="control-label qt-or-label">~ or ~</div>
-        {character && JSON.parse(character.value).characterClass ? (
-          <button onClick={dispatchCharacterClass}>
-            {getClass(character)?.title}
-          </button>
-        ) : (
-          <DropdownMenu 
-            dropdownType="class" 
-            menu={classMenu(expansion, faction, characterClass)} 
-          />
-        )}
-        {character && JSON.parse(character.value).race ? (
-          <button onClick={dispatchCharacterRace}>
-            {getRace(character, faction)?.title}
-          </button>
-        ) : (
-          <DropdownMenu dropdownType="race" menu={raceMenu(expansion, faction, race)} />
-        )}
+        <DropdownMenu
+          dropdownType="class"
+          menu={classMenu(expansion, faction, characterClass)}
+        />
+        <DropdownMenu dropdownType="race" menu={raceMenu(expansion, faction, race)} />
       </div>
       <div className="qt-additional-filters">
         <div className="control-label qt-additional-label">Additional Filters:</div>
-        <DropdownMenu 
-          dropdownType="character" 
-          menu={characterMenu(character, characters, faction)} 
+        <DropdownMenu
+          dropdownType="character"
+          menu={characterMenu(character, characters, faction)}
         />
         <DropdownMenu dropdownType="type" menu={questTypeMenu(expansion, type)} />
       </div>

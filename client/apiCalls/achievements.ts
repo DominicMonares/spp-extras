de974feaa793f6239713577793fda7cdc6e82eac
akaClay <domakaclay@gmail.com>
2023-04-01 03:30:44 -0700

    temporarily disable websocket

--- client/apiCalls/achievements.ts
+++ client/apiCalls/achievements.ts
@@ -1,27 +1,27 @@
 import { port, wsUrl } from '../config';
 
 
-const gameSocket = new WebSocket(`${wsUrl}:${port}/ws/achievements/account_wide/`);
+// const gameSocket = new WebSocket(`${wsUrl}:${port}/ws/achievements/account_wide/`);
 
 const connect = () => {
-  gameSocket.onopen = () => {
-    console.log('WebSocket connection created!');
-  }
+  // gameSocket.onopen = () => {
+  //   console.log('WebSocket connection created!');
+  // }
 
-  gameSocket.onclose = () => {
-    console.log('WebSocket connection closed!');
-  }
+  // gameSocket.onclose = () => {
+  //   console.log('WebSocket connection closed!');
+  // }
 
-  gameSocket.onmessage = (e: any) => { // TEMP ANY
-    const data = JSON.parse(e.data);
-    const message = data.message;
-    console.log('FINAL MESSAGE ', message);
-  }
+  // gameSocket.onmessage = (e: any) => { // TEMP ANY
+  //   const data = JSON.parse(e.data);
+  //   const message = data.message;
+  //   console.log('FINAL MESSAGE ', message);
+  // }
 }
 
 export const fetchAchievements: any = async (expansion: any) => { // TEMP ANY
-  connect();
-  gameSocket.send(JSON.stringify({
-    'message': 'WORK YOU SUNUVABITCH!!!'
-  }));
+  // connect();
+  // gameSocket.send(JSON.stringify({
+  //   'message': 'WORK YOU SUNUVABITCH!!!'
+  // }));
 }

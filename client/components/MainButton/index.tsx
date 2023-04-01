25fd2064205a0c5b40801c30b87ca72d3cab40aa
akaClay <domakaclay@gmail.com>
2023-04-01 01:25:45 -0700

    remove hovering from state

--- client/components/MainButton/index.tsx
+++ client/components/MainButton/index.tsx
@@ -1,25 +1,18 @@
-import { useState } from 'react';
 import { MainButtonProps } from '../../types';
 import './MainButton.css';
 
 
 const MainButton = ({ active, handleClick, buttonText }: MainButtonProps) => {
-  const [hovering, setHovering] = useState<string>('');
-
   return (
     <button
       className={`
         main-button
-        ${hovering ? 'main-hovering' : ''}
-        ${active ? 'main-active' : ''}
+        ${active ? 'main-button-active' : ''}
       `}
       onClick={handleClick}
-      onMouseEnter={() => setHovering('-hovering')}
-      onMouseLeave={() => setHovering('')}
     >
       <div className={`
         main-button-text
-        ${hovering ? 'main-text-hovering' : ''}
         ${active ? 'main-text-active' : ''}
       `}>
         {buttonText}

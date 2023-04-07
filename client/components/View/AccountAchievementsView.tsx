bba1084b89eb5aab7a2d74940789b45a3a0f14a5
akaClay <domakaclay@gmail.com>
2023-04-07 03:01:25 -0700

    add messages

--- client/components/View/AccountAchievementsView.tsx
+++ client/components/View/AccountAchievementsView.tsx
@@ -1,10 +1,21 @@
 import MainBigHeader from '../MainBigHeader';
+import { useAppSelector } from '../../store/hooks';
 
 
 const AccountAchievementsView = () => {
+  const messages = useAppSelector(state => state.websocket.messages);
+
   return (
     <div>
-      <MainBigHeader headerText="Share achievements between all characters" /> 
+      {!messages.length ? (
+        <ol>
+          {messages.map((m: string) => {
+            return <li>{m}</li>
+          })}
+        </ol>
+      ) : (
+        <MainBigHeader headerText="Share achievements between all characters" /> 
+      )}
     </div>
   );
 }

f8cd5d7e185503ad94bdba45847a71f744240894
akaClay <domakaclay@gmail.com>
2023-04-07 01:50:48 -0700

    loading text styling

--- client/components/MainBigHeader/index.tsx
+++ client/components/MainBigHeader/index.tsx
@@ -6,7 +6,12 @@ const MainBigHeader = ({ headerText }) => {
   return (
     <div className="main-big-header">
       <img src={header} />
-      <div className="main-big-header-text">{headerText}</div>
+      <div className={`
+        main-big-header-text 
+        ${headerText.includes('Loading') ? 'loading-text' : ''}
+      `}>
+        {headerText}
+      </div>
     </div>
   );
 }

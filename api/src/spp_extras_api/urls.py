a285a58fd4100e1406c0b353b25c0414676843e5
akaClay <domakaclay@gmail.com>
2023-04-08 16:04:57 -0700

    change data to all_data

--- api/src/spp_extras_api/urls.py
+++ api/src/spp_extras_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views.data import DataViewSet
+from .views.all_data import DataViewSet
 
 
 router = routers.DefaultRouter()

e853164bad5602cf6140a551f680eb14881f4f53
akaClay <domakaclay@gmail.com>
2023-04-08 02:05:18 -0700

    message view styling

--- client/components/View/MessageView.tsx
+++ client/components/View/MessageView.tsx
@@ -1,14 +1,16 @@
 import MainBigHeader from '../MainBigHeader';
 import { useAppSelector } from '../../store/hooks';
+import './View.css';
 
 
 const MessageView = () => {
+  const expansion = useAppSelector(state => state.expansion.selected);
   const messages = useAppSelector(state => state.websocket.messages);
 
   return (
     <div>
       {messages.length ? (
-        <ol className="">
+        <ol className={`messages ${expansion}-messages`}>
           {messages.map((m: string, i) => {
             return <li key={i}>{m}</li>
           })}

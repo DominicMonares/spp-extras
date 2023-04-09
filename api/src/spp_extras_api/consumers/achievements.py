5c4da8607f7dbbc797ae09e8318a9f3e46242639
akaClay <domakaclay@gmail.com>
2023-04-09 04:17:36 -0700

    improve error handling

--- api/src/spp_extras_api/consumers/achievements.py
+++ api/src/spp_extras_api/consumers/achievements.py
@@ -38,15 +38,21 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
         try:
             send_msg('Looking for shared achievement progress table...')
             casp_table = char_achievement_shared_prog_exists()
-            if not casp_table:
+            if casp_table:
+                send_msg('Shared achievement progress table found!')
+            else:
                 send_msg('Shared achievement progress table not found!')
                 send_msg('Creating shared achievement progress table...')
-                create_char_achievement_shared_prog()
-                send_msg('Shared achievement progress table successfully created!')
-            else:
-                send_msg('Shared achievement progress table found!')
-        except:
-            send_msg('Unable to find/create the shared achievement progress table!')
+                try:
+                    create_char_achievement_shared_prog()
+                    send_msg('Shared achievement progress table successfully created!')
+                except Exception as e:
+                    send_msg('Failed to create the shared achievement progress table!')
+                    send_msg(f'Error: {e}')
+                    return
+        except Exception as e:
+            send_msg('Failed to find the shared achievement progress table!')
+            send_msg(f'Error: {e}')
             return
 
         # Restore cut titles if they don't exist in db
@@ -58,10 +64,16 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             else:
                 send_msg('Cut titles not found!')
                 send_msg('Adding cut titles to the database...')
-                ins_cut_titles()
-                send_msg('Cut titles successfully added to the database!')
-        except:
-            send_msg('Unable to find/add cut titles!')
+                try:
+                    ins_cut_titles()
+                    send_msg('Cut titles successfully added to the database!')
+                except Exception as e:
+                    send_msg('Failed to add cut titles to database!')
+                    send_msg(f'Error: {e}')
+                    return
+        except Exception as e:
+            send_msg('Failed to find cut titles!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch all account data
@@ -69,8 +81,9 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching account data...')
             accounts = sel_all_account_data('wotlk')
             send_msg('Account data successfully fetched!')
-        except:
-            send_msg('Unable to fetch account data!')
+        except Exception as e:
+            send_msg('Failed to fetch account data!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch all character data
@@ -78,8 +91,9 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching character data...')
             characters = sel_all_char_data('wotlk')
             send_msg('Character data successfully fetched!')
-        except:
-            send_msg('Unable to fetch character data!')
+        except Exception as e:
+            send_msg('Failed to fetch character data!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch all character achievement data
@@ -87,8 +101,9 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching character achievement data...')
             achievements = sel_all_char_achievements()
             send_msg('Character achievement data successfully fetched!')
-        except:
-            send_msg('Unable to fetch character achievement data!')
+        except Exception as e:
+            send_msg('Failed to fetch character achievement data!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch all completed regular quest data
@@ -96,8 +111,9 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching completed regular quest data...')
             completed_regular = sel_all_completed_reg_quests('wotlk')
             send_msg('Completed regular quest data successfully fetched!')
-        except:
-            send_msg('Unable to fetch completed regular quest data!')
+        except Exception as e:
+            send_msg('Failed to fetch completed regular quest data!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch all completed daily quest data
@@ -105,8 +121,9 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching completed daily quest data...')
             completed_daily = sel_all_completed_daily_quests('wotlk')
             send_msg('Completed daily quest data successfully fetched!')
-        except:
-            send_msg('Unable to fetch completed daily quest data!')
+        except Exception as e:
+            send_msg('Failed to fetch completed daily quest data!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch template quests
@@ -114,8 +131,9 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching template quest data...')
             template_quests = sel_all_template_quests('wotlk')
             send_msg('Template quest data successfully fetched!')
-        except:
-            send_msg('Unable to fetch template quest data!')
+        except Exception as e:
+            send_msg('Failed to fetch template quest data!')
+            send_msg(f'Error: {e}')
             return
 
         # Fetch all shared achievements progress
@@ -123,6 +141,7 @@ class AccountWideAchievementsConsumer(WebsocketConsumer):
             send_msg('Fetching all shared achievement progress data...')
             shared_achievement_prog = sel_all_char_achievement_shared_prog()
             send_msg('Shared achievement progress data successfully fetched!')
-        except:
-            send_msg('Unable to fetch shared achievement progress data!')
+        except Exception as e:
+            send_msg('Failed to fetch shared achievement progress data!')
+            send_msg(f'Error: {e}')
             return

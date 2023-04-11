c1ad984763bd5fa9606961bcfa17c05af31fa3d2
akaClay <domakaclay@gmail.com>
2023-04-10 23:10:21 -0700

    complete combine_char_data

--- api/src/spp_extras_api/utils/achievements.py
+++ api/src/spp_extras_api/utils/achievements.py
@@ -54,19 +54,47 @@ def combine_char_data(
     achievement_shared_prog,
     completed_quests
 ):
+    # Use characters as base for all char data
     all_char_data = characters
+
+    # Iterate through all accounts and characters to add data
     for acct_id in all_char_data:
-        acct_chars = all_char_data[acct_id]['characters']
-        acct_achievement_progress = achievement_shared_prog[acct_id]
-        all_char_data[acct_id]['achievement_shared_prog'] = acct_achievement_progress
-        for faction in acct_chars:
-            for char_id in acct_chars[faction]:
-                char = acct_chars[faction][char_id]
-                credit = achievement_credit[char]
-                progress = achievement_prog[char]
-                quests = completed_quests[char]
-                all_char_data[acct_id][char][faction]['achievement_credit'] = credit
-                all_char_data[acct_id][char][faction]['achievement_prog'] = progress
-                all_char_data[acct_id][char][faction]['quests'] = quests
+        chars = all_char_data[acct_id]['characters']
+        credit = { 'alliance': {}, 'horde': {} }
+        shared_progress = achievement_shared_prog[acct_id]
+        quests = { 'regular': {}, 'daily': {} }
+        for faction in chars:
+            for char_id in chars[faction]:
+                # Add to account-wide achievement credit
+                for achievement_id in achievement_credit[char_id]:
+                    achievement = credit[faction][achievement_id]
+                    existing_date = achievement['date']
+                    incoming_date = achievement_credit[char_id][achievement_id]['date']
+                    older_entry = existing_date > incoming_date
+
+                    # Use oldest completion date
+                    if not achievement or older_entry:
+                        achievement = achievement_credit[char_id][achievement_id]
+                        credit[faction][achievement_id] = achievement
+
+                # Add to account-wide quest credit
+                for quest_id in completed_quests[char_id]['regular']:
+                    quest = completed_quests[char_id]['regular'][quest_id]
+                    if not quests['regular'][quest_id]:
+                        quests['regular'][quest_id] = quest
+
+                for quest_id in completed_quests[char_id]['daily']:
+                    quest = completed_quests[char_id]['daily'][quest_id]
+                    if not quests['daily'][quest_id]:
+                        quests['daily'][quest_id] = quest
+                
+                # Add all data to individual characters
+                progress = achievement_prog[char_id]
+                all_char_data[acct_id]['characters'][faction][char_id]['progress'] = progress
+
+        # Add all data to accounts
+        all_char_data[acct_id]['credit'] = credit
+        all_char_data[acct_id]['shared_progress'] = shared_progress
+        all_char_data[acct_id]['quests'] = quests
 
     return all_char_data

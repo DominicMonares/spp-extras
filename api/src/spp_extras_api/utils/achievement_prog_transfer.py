57b18d74656584fe43b8c54f315b1b39534352e0
akaClay <domakaclay@gmail.com>
2023-04-20 22:09:17 -0700

    save loremaster progress

--- api/src/spp_extras_api/utils/achievement_prog_transfer.py
+++ api/src/spp_extras_api/utils/achievement_prog_transfer.py
@@ -1,6 +1,6 @@
 import json
 from from_root import from_root
-from spp_extras_api.utils.loremaster import loremaster
+from spp_extras_api.utils.loremaster import loremaster, misc_lm_criteria
 with open(from_root('data/factionAchievements.json'), 'r') as json_file:
     faction_achievements = json.load(json_file)
 with open(from_root('data/loremasterAchCriteria.json'), 'r') as json_file:
@@ -24,8 +24,26 @@ def create_prog_args(all_chars, template_quests):
         credit = acct['credit']
         shared_progress = acct['shared_progress']
         completed_quests = acct['quests']['regular']
+        loremaster_count = {
+            'alliance_ek': {
+                'count': 0,
+                'date': 0000000000
+            }, 
+            'alliance_k': {
+                'count': 0,
+                'date': 0000000000
+            }, 
+            'horde_ek': {
+                'count': 0,
+                'date': 0000000000
+            }, 
+            'horde_k': {
+                'count': 0,
+                'date': 0000000000
+            }
+        }
 
-        # Organize all character achievement progress by criteria
+        # Organize all character shared achievement progress by criteria
         new_shared_prog = {}
 
         def add_to_shared_prog(criteria_id, ach_prog):
@@ -33,6 +51,14 @@ def create_prog_args(all_chars, template_quests):
                 new_shared_prog[criteria_id] = [ach_prog]
             else:
                 new_shared_prog[criteria_id].append(ach_prog)
+
+        def create_char_prog_args(guid, criteria, counter, date):
+            args['char_prog_args'].append({
+                'guid': guid,
+                'criteria': int(criteria),
+                'counter': counter,
+                'date': date
+            })
 
         # Add existing progress for each char to new_shared_prog
         for char_id in chars:
@@ -46,52 +72,53 @@ def create_prog_args(all_chars, template_quests):
         # Run transfers for all shared progress
         for criteria_id in new_shared_prog:
             ach_prog = new_shared_prog[criteria_id]
-            # if not len(ach_prog): continue
             date = ach_prog[0]['date']
+            previous_count = 0
             new_count = 0
+            new_progress = 0
 
-            # Loremaster progress calculated separately from the rest
-            if criteria_id not in loremaster_ach_criteria: 
-                previous_count = 0
-                new_progress = 0
+            # Re-assign previous count and date if shared progress already exists
+            acct_shared_exists = len(shared_progress) > 0
+            acct_shared_criteria_exists = False
+            if acct_shared_exists: 
+                criteria_exists = criteria_id in shared_progress
+                acct_shared_criteria_exists = criteria_exists
+            
+            if acct_shared_criteria_exists:
+                shared_criteria = shared_progress[criteria_id]
+                previous_count = shared_criteria['counter']
+                date = shared_criteria['date']
+            
+            # Calculate new count using previous count
+            for char_ach_prog in ach_prog:
+                # Use most recent date for progress
+                char_prog_date = char_ach_prog['date']
+                if char_prog_date > date: 
+                    date = char_prog_date
+                
+                # Add to new progress count
+                char_prog_count = char_ach_prog['counter']
+                count = char_prog_count - previous_count
+                if count >= 0:
+                    new_progress += count
+                else:
+                    new_progress += char_prog_count
+            
+            # Create final count sum
+            new_count = previous_count + new_progress
 
-                # Re-assign previous count and date if shared progress already exists
-                acct_shared_exists = len(shared_progress) > 0
-                acct_shared_criteria_exists = False
-                if acct_shared_exists: 
-                    criteria_exists = criteria_id in shared_progress
-                    acct_shared_criteria_exists = criteria_exists
-                
-                if acct_shared_criteria_exists:
-                    shared_criteria = shared_progress[criteria_id]
-                    previous_count = shared_criteria['counter']
-                    date = shared_criteria['date']
-                
-                # Calculate new count using previous count
-                for char_ach_prog in ach_prog:
-                    # Use most recent date for progress
-                    char_prog_date = char_ach_prog['date']
-                    if char_prog_date > date: 
-                        date = char_prog_date
-                    
-                    # Add to new progress count
-                    char_prog_count = char_ach_prog['counter']
-                    count = char_prog_count - previous_count
-                    if count >= 0:
-                        new_progress += count
-                    else:
-                        new_progress += char_prog_count
-                
-                # Create final count sum
-                new_count = previous_count + new_progress
+            # Add misc Loremaster criteria to loremaster_count if it exists
+            misc_lm_crit = misc_lm_criteria(criteria_id)
+            if misc_lm_crit:
+                loremaster_count[misc_lm_crit] += 1
 
-                # Transfer shared achievement progress
-                args['shared_prog_args'].append({
-                    'account': acct_id,
-                    'criteria': int(criteria_id),
-                    'counter': new_count,
-                    'date': date
-                })
+            # Transfer shared achievement progress
+            args['shared_prog_args'].append({
+                'account': acct_id,
+                'criteria': int(criteria_id),
+                'counter': new_count,
+                'date': date
+            })
 
             # Ensure progress counter doesn't exceed threshold
             threshold = shared_ach_criteria[criteria_id]['threshold']
@@ -105,14 +132,14 @@ def create_prog_args(all_chars, template_quests):
                 credit[ach_id] = date
 
             # Transfer individual character progress
-            # Make sure you don't add loremaster achievements to wrong faction!
             for char_id in chars:
-                args['char_prog_args'].append({
-                    'guid': char_id,
-                    'criteria': int(criteria_id),
-                    'counter': new_count,
-                    'date': date
-                })
+                create_char_prog_args(char_id, criteria_id, new_count, date)
+
+        # Run transfers for Loremaster progress
+        loremaster_count = loremaster(completed_quests, template_quests, loremaster_count)
+        for char_id in chars:
+            create_char_prog_args(char_id, criteria_id, new_count, date)
+        
 
 
     # MAY NOT NEED DAILIES IN CHAR OBJ
--- api/src/spp_extras_api/utils/loremaster.py
+++ api/src/spp_extras_api/utils/loremaster.py
@@ -4,22 +4,73 @@ with open(from_root('data/zoneContinents.json'), 'r') as json_file:
     zone_continents = json.load(json_file)
 
 
-def loremaster(completed_quests, template_quests, faction):
-    faction_quests = template_quests[faction]
+# See which completed quests belong to which criteria and what their counts are
+def loremaster(completed_quests, template_quests, loremaster_prog):
+    alliance_template_quests = template_quests['alliance']
+    horde_template_quests = template_quests['horde']
     neutral_quests = template_quests['both']
-    all_template_quests = {**faction_quests, **neutral_quests}
-    eastern_kingdoms_count = 0
-    kalimdor_count = 0
+    all_alliance_template_quests = {**alliance_template_quests, **neutral_quests}
+    all_horde_template_quests = {**horde_template_quests, **neutral_quests}
 
+    # Track highest counts and most recent dates for main Loremaster quests
+    # Use counts from misc Loremaster criteria as a starting point
+    alliance_ek = loremaster_prog['alliance_ek']
+    alliance_ek_count = alliance_ek['count']
+    alliance_ek_date = alliance_ek['date']
+    alliance_k = loremaster_prog['alliance_k']
+    alliance_k_count = alliance_k['count']
+    alliance_k_date = alliance_k['date']
+    horde_ek = loremaster_prog['horde_ek']
+    horde_ek_count = horde_ek['count']
+    horde_ek_date = horde_ek['date']
+    horde_k = loremaster_prog['horde_k']
+    horde_k_count = horde_k['count']
+    horde_k_date = horde_k['date']
+
+    # Track criteria counts for each faction/zone
+    all_criteria = {}
+    
     for quest_id in completed_quests:
-        if quest_id in all_template_quests:
-            template_quest = all_template_quests[quest_id]
-            zoneorsort = str(template_quest['zoneorsort'])
-            if zoneorsort in zone_continents:
-                continent = zone_continents[zoneorsort]
-                if continent == 0:
-                    eastern_kingdoms_count += 1
-                else:
-                    kalimdor_count += 1
+        if quest_id in all_alliance_template_quests:
+            
 
-    return [eastern_kingdoms_count, kalimdor_count]
+        if quest_id in all_horde_template_quests:
+
+
+
+    # for quest_id in completed_quests:
+    #     if quest_id in all_template_quests:
+    #         template_quest = all_template_quests[quest_id]
+    #         zoneorsort = str(template_quest['zoneorsort'])
+    #         if zoneorsort in zone_continents:
+    #             continent = zone_continents[zoneorsort]
+    #             if continent == 0:
+    #                 eastern_kingdoms_count += 1
+    #             else:
+    #                 kalimdor_count += 1
+
+    # return [eastern_kingdoms_count, kalimdor_count]
+
+    # Return list of criteria to add for each char AND totals for each achievement
+
+
+# Some criteria for Loremaster achievements don't have matching zonerefs or have duplicates
+# Progress for these criteria are tracked separately from the other Loremaster criteria
+# and are tracked like shared achievements
+# There will likely be side-effects to achievements tracked this way since there's no 
+# way to check for duplicate progress, but the alternative is risking achievement credit
+# not being given on transfer
+def misc_lm_criteria(criteria_id):
+    alliance_ek = [5903, 5910, 9398, 9424, 5927, 5928, 5940, 5944, 9422]
+    alliance_k = [7895, 6017, 6021, 6030]
+    horde_ek = [5955, 5962, 9425, 5979, 5980, 5992, 5996, 9423]
+    horde_k = [7899, 6112, 6116, 6125]
+
+    if criteria_id in alliance_ek:
+        return 'alliance_ek'
+    elif criteria_id in alliance_k:
+        return 'alliance_k'
+    elif criteria_id in horde_ek:
+        return 'horde_ek'
+    elif criteria_id in horde_k:
+        return 'horde_k'

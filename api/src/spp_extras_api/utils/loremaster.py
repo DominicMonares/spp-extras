72fbb92c22296ef4b1b00e17c8d620bb5f3846de
akaClay <domakaclay@gmail.com>
2023-04-23 04:10:49 -0700

    cleanup

--- api/src/spp_extras_api/utils/loremaster.py
+++ api/src/spp_extras_api/utils/loremaster.py
@@ -11,8 +11,10 @@ def loremaster(completed_quests, template_quests, loremaster_prog):
     alliance_template_quests = template_quests['alliance']
     horde_template_quests = template_quests['horde']
     neutral_quests = template_quests['both']
-    all_alliance_template_quests = {**alliance_template_quests, **neutral_quests}
-    all_horde_template_quests = {**horde_template_quests, **neutral_quests}
+    all_alliance_template_quests = {
+        **alliance_template_quests, **neutral_quests}
+    all_horde_template_quests = {
+        **horde_template_quests, **neutral_quests}
 
     # Track highest counts and most recent dates for main Loremaster quests
     # Use counts from misc Loremaster criteria as a starting point
@@ -31,9 +33,24 @@ def loremaster(completed_quests, template_quests, loremaster_prog):
 
     # Track criteria counts and dates for each faction/zone
     all_criteria = {'alliance': {}, 'horde': {}}
-    
+
+    def count_sub_criteria(criteria_id, date, faction):
+        if criteria_id not in all_criteria[faction]:
+            all_criteria[faction][criteria_id] = {
+                'count': 1,
+                'date': date
+            }
+        else:
+            all_criteria[faction][criteria_id]['count'] += 1
+            existing_date = all_criteria[faction][criteria_id]['date']
+            if existing_date < date:
+                all_criteria[faction][criteria_id]['date'] = date
+
+
     for quest_id in completed_quests:
         date = completed_quests[quest_id]['timer']
+
+        # Count Alliance progress
         if quest_id in all_alliance_template_quests:
             template_quest = all_alliance_template_quests[quest_id]
             zone_id = str(template_quest['zoneorsort'])
@@ -41,20 +58,9 @@ def loremaster(completed_quests, template_quests, loremaster_prog):
                 zone_criteria = lm_criteria['alliance'][zone_id]
                 criteria_id = str(zone_criteria['criteria'])
                 ach_id = zone_criteria['achievement']
+                count_sub_criteria(criteria_id, date, 'alliance')
 
-                # Add counter for criteria if it doesn't exist
-                if criteria_id not in all_criteria['alliance']:
-                    all_criteria['alliance'][criteria_id] = {
-                        'count': 1,
-                        'date': date
-                    }
-                else:
-                    all_criteria['alliance'][criteria_id]['count'] += 1
-                    existing_date = all_criteria['alliance'][criteria_id]['date']
-                    if existing_date < date:
-                        all_criteria['alliance'][criteria_id]['date'] = date
-                    
-                # Add to main counters 
+                # Add to main counters
                 if ach_id == 1676:
                     alliance_ek_count += 1
                     if alliance_ek_date < date:
@@ -64,6 +70,7 @@ def loremaster(completed_quests, template_quests, loremaster_prog):
                     if alliance_k_date < date:
                         alliance_k_date = date
 
+        # Count Horde progress
         if quest_id in all_horde_template_quests:
             template_quest = all_horde_template_quests[quest_id]
             zone_id = str(template_quest['zoneorsort'])
@@ -71,18 +78,7 @@ def loremaster(completed_quests, template_quests, loremaster_prog):
                 zone_criteria = lm_criteria['horde'][zone_id]
                 criteria_id = str(zone_criteria['criteria'])
                 ach_id = zone_criteria['achievement']
-
-                # Add counter for criteria if it doesn't exist
-                if criteria_id not in all_criteria['horde']:
-                    all_criteria['horde'][criteria_id] = {
-                        'count': 1,
-                        'date': date
-                    }
-                else:
-                    all_criteria['horde'][criteria_id]['count'] += 1
-                    existing_date = all_criteria['horde'][criteria_id]['date']
-                    if existing_date < date:
-                        all_criteria['horde'][criteria_id]['date'] = date
+                count_sub_criteria(criteria_id, date, 'horde')
 
                 # Add to main counters
                 if ach_id == 1677:
@@ -95,44 +91,32 @@ def loremaster(completed_quests, template_quests, loremaster_prog):
                         horde_k_date = date
 
     new_loremaster_prog = {
-        '1676': {
-            'count': alliance_ek_count,
-            'date': alliance_ek_date
-        },
-        '1678': {
-            'count': alliance_k_count,
-            'date': alliance_k_date
-        },
-        '1677': {
-            'count': horde_ek_count,
-            'date': horde_ek_date
-        },
-        '1680': {
-            'count':horde_k_count,
-            'date': horde_k_date
-        }
+        '1676': {'count': alliance_ek_count, 'date': alliance_ek_date},
+        '1678': {'count': alliance_k_count, 'date': alliance_k_date},
+        '1677': {'count': horde_ek_count, 'date': horde_ek_date},
+        '1680': {'count': horde_k_count, 'date': horde_k_date}
     }
 
     all_progress = {
         'main_prog': new_loremaster_prog,
         'sub_prog': all_criteria
     }
-    
+
     return all_progress
 
 
 # Some criteria for Loremaster achievements don't have matching zonerefs or have duplicates
 # Progress for these criteria are tracked separately from the other Loremaster criteria
 # and are tracked like shared achievements
-# There will likely be side-effects to achievements tracked this way since there's no 
+# There will likely be side-effects to achievements tracked this way since there's no
 # way to check for duplicate progress, but the alternative is risking achievement credit
-# not being given on transfer
+# not being given on transfer at all
 def misc_lm_criteria(criteria_id):
     alliance_ek = [5903, 5910, 9398, 9424, 5927, 5928, 5940, 5944, 9422]
     alliance_k = [7895, 6017, 6021, 6030]
     horde_ek = [5955, 5962, 9425, 5979, 5980, 5992, 5996, 9423]
     horde_k = [7899, 6112, 6116, 6125]
-
+    
     if criteria_id in alliance_ek:
         return 'alliance_ek'
     elif criteria_id in alliance_k:

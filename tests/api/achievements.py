939f1c863275c89868dd1839773de4b832785b58
akaClay <domakaclay@gmail.com>
2023-04-24 01:23:01 -0700

    implement ach progress tests

--- tests/api/achievements.py
+++ tests/api/achievements.py
@@ -11,21 +11,35 @@ from api.src.spp_extras_api.utils.achievements import (
 )
 with open(from_root('tests/samples/achCredit.json'), 'r') as json_file:
     ach_credit = json.load(json_file)
+with open(from_root('tests/samples/achProgress.json'), 'r') as json_file:
+    ach_prog = json.load(json_file)
 with open(from_root('tests/samples/rawAchCredit.json'), 'r') as json_file:
     raw_ach_credit = json.load(json_file)
+with open(from_root('tests/samples/rawAchProgress.json'), 'r') as json_file:
+    raw_ach_prog = json.load(json_file)
 
 
 class TestFormatAchCredit(TestCase):
-    """Should return achievement credit formatted by character"""
+    """Should return achievement credit organized by character"""
     def test_format_ach_credit(self):
         result = format_ach_credit(raw_ach_credit)
         self.assertDictEqual(result, ach_credit)
 
 
 class TestFormatAchProg(TestCase):
-    """PLACEHOLDER"""
+    """Should return achievement progress organized by account"""
     def test_format_ach_prog(self):
-        self.assertDictEqual({}, {})
+        raw_acct_prog = raw_ach_prog['account']
+        acct_prog = ach_prog['account']
+        result = format_ach_prog('shared', raw_acct_prog)
+        self.assertDictEqual(result, acct_prog)
+
+    """Should return achievement progress organized by character"""
+    def test_format_ach_prog(self):
+        raw_char_prog = raw_ach_prog['character']
+        char_prog = ach_prog['character']
+        result = format_ach_prog('char', raw_char_prog)
+        self.assertDictEqual(result, char_prog)
 
 
 class TestCombineCharData(TestCase):

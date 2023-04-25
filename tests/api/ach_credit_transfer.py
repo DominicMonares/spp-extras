e0e57e0a810f2acc3063c69c1cb7b4eb8508c0fe
akaClay <domakaclay@gmail.com>
2023-04-25 07:35:06 -0700

    save ach transfer test progress

--- tests/api/ach_credit_transfer.py
+++ tests/api/ach_credit_transfer.py
@@ -2,11 +2,20 @@ import json
 from from_root import from_root
 from unittest import TestCase
 from api.src.spp_extras_api.utils.ach_credit_transfer import transfer_ach_credit
-with open(from_root('tests/samples/completedQuests.json'), 'r') as json_file:
-    completed_quests = json.load(json_file)
+with open(from_root('tests/samples/achCreditTransfer.json'), 'r') as json_file:
+    expected = json.load(json_file)
+with open(from_root('tests/samples/achProgTransfer.json'), 'r') as json_file:
+    ach_prog_transfer = json.load(json_file)
+with open(from_root('tests/samples/achRewards.json'), 'r') as json_file:
+    ach_rewards = json.load(json_file)
+with open(from_root('tests/samples/itemCharges.json'), 'r') as json_file:
+    item_charges = json.load(json_file)
 
 
 class TestTransferAchCredit(TestCase):
-    """PLACEHOLDER"""
+    """Should return all credit and reward arguments"""
     def test_transfer_ach_credit(self):
-        self.assertDictEqual({}, {})
+        chars = ach_prog_transfer['new_chars']
+        result = transfer_ach_credit(chars, ach_rewards, item_charges, 1000, 1000)
+        print(f'OIIIIIII {result}')
+        self.assertDictEqual(result, expected)

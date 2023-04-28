6e29eb29be4d38e55acea0b531efc2da99697614
akaClay <domakaclay@gmail.com>
2023-04-28 03:22:52 -0700

    implement all functions

--- api/src/spp_extras_api/consumers/reputations.py
+++ api/src/spp_extras_api/consumers/reputations.py
@@ -1,8 +1,13 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from spp_extras_api.queries.characters import (
-    sel_all_char_rep
+    sel_all_char_rep, 
+    sel_all_chars,
+    upd_char_rep
 )
+from spp_extras_api.queries.realmd import sel_all_accounts
+from spp_extras_api.utils.characters import format_accts_n_chars, format_player_accts
+from spp_extras_api.utils.reputations import format_reputations, transfer_reputations
 
 
 class AccountWideReputationsConsumer(WebsocketConsumer):
@@ -15,21 +20,76 @@ class AccountWideReputationsConsumer(WebsocketConsumer):
         pass
 
     # Run all reputation sharing functions when account-wide rep message is received
-    # Message contents determine whether to run for players and bots or just players
+    # Only shared for player accounts - message contents do not matter
     def receive(self, text_data):
         def send_msg(msg): self.send(json.dumps({'message': msg}))
-        bots_active = True
-        message_data = json.loads(text_data)
-        message = message_data['message']
-        if message == 'player':
-            bots_active = False
 
-        # Fetch character reputation data
+        # Fetch all account data
+        try:
+            send_msg('Fetching account data...')
+            account_data = sel_all_accounts('wotlk')
+            send_msg('Account data successfully fetched!')
+        except Exception as e:
+            send_msg('Failed to fetch account data!')
+            send_msg(f'Error: {e}')
+            return
+
+        # Fetch all character data
+        try:
+            send_msg('Fetching character data...')
+            character_data = sel_all_chars('wotlk')
+            send_msg('Character data successfully fetched!')
+        except Exception as e:
+            send_msg('Failed to fetch character data!')
+            send_msg(f'Error: {e}')
+            return
+
+        # Combine all player accounts/chars then fetch reputation data
         try:
             send_msg('Fetching character reputation data...')
-            char_rep_data = sel_all_char_rep('wotlk')
+            _accounts = format_accts_n_chars(account_data, character_data)
+            accounts = format_player_accts(_accounts, False)
+            char_ids = []
+            for c in accounts['characters']:
+                char_ids.append(int(c))
+            char_rep_data = sel_all_char_rep('wotlk', char_ids)
             send_msg('Character reputation data successfully fetched!')
         except Exception as e:
             send_msg('Failed to fetch character reputation data!')
             send_msg(f'Error: {e}')
             return
+
+        # Format fetched reputation data
+        try:
+            send_msg('Formatting fetched data...')
+            reputations = format_reputations(char_rep_data)
+            send_msg('Fetched data successfully formatted!')
+        except Exception as e:
+            send_msg('Failed to format fetched data!')
+            send_msg(f'Error: {e}')
+            return
+
+        # Run reputation progress transfer
+        try:
+            send_msg('Transferring reputation progress between characters...')
+            rep_args = transfer_reputations(reputations)
+            send_msg(
+                'Reputation progress successfully transferred between characters!')
+        except Exception as e:
+            send_msg('Failed to transfer reputation progress between characters!')
+            send_msg(f'Error: {e}')
+            return
+
+        # Save new character reputation progress
+        if len(rep_args):
+            try:
+                send_msg('Saving new reputation data...')
+                upd_char_rep(rep_args)
+                send_msg('New reputation data successfully saved!')
+            except Exception as e:
+                send_msg('Failed to save new reputation data!')
+                send_msg(f'Error: {e}')
+                return
+
+        send_msg('All reputation processes finished!')
+        send_msg('You can safely close this tool now.')

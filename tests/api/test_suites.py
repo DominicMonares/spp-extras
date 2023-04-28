c31e68a8242ae30e48d90604096d3791a1692c36
akaClay <domakaclay@gmail.com>
2023-04-28 05:03:19 -0700

    add reputation suite

--- tests/api/test_suites.py
+++ tests/api/test_suites.py
@@ -13,7 +13,10 @@ from .characters import (
     TestCheckFaction,
     TestFormatCharacters,
     TestFormatPlayers,
-    TestFormatReputations
+)
+from .reputations import (
+    TestFormatReputations,
+    TestTransferReputations
 )
 from .quests import TestFormatCompletedQuests, TestFormatTemplateQuests
 
@@ -27,9 +30,11 @@ def suites():
     # Achievement Credit Transfer Utils
     transfer_ach_credit_suite = TestLoader()\
         .loadTestsFromTestCase(TestTransferAchCredit)
+    
     # Achievement Progress Transfer Utils
     transfer_ach_prog_suite = TestLoader()\
         .loadTestsFromTestCase(TestTransferAchProg)
+    
     # Achievement Utils
     format_all_acct_data_suite = TestLoader()\
         .loadTestsFromTestCase(TestCombineAcctData)
@@ -43,6 +48,7 @@ def suites():
         .loadTestsFromTestCase(TestFormatRewItemCharges)
     check_faction_ach_suite = TestLoader()\
         .loadTestsFromTestCase(TestCheckFactionAch)
+    
     # Character Utils
     check_faction_suite = TestLoader()\
         .loadTestsFromTestCase(TestCheckFaction)
@@ -50,13 +56,19 @@ def suites():
         .loadTestsFromTestCase(TestFormatCharacters)
     format_players_suite = TestLoader()\
         .loadTestsFromTestCase(TestFormatPlayers)
+    
+    # Reputation Utils
     format_char_reps_suite = TestLoader()\
         .loadTestsFromTestCase(TestFormatReputations)
+    transfer_reputations_suite = TestLoader()\
+        .loadTestsFromTestCase(TestTransferReputations)
+    
     # Quest Utils
     format_completed_quests_suite = TestLoader()\
         .loadTestsFromTestCase(TestFormatCompletedQuests)
     format_template_quests_suite = TestLoader()\
         .loadTestsFromTestCase(TestFormatTemplateQuests)
+    
     # Top Level Suite
     top_suite = TestSuite([
         transfer_ach_credit_suite,
@@ -71,6 +83,7 @@ def suites():
         format_characters_suite,
         format_players_suite,
         format_char_reps_suite,
+        transfer_reputations_suite,
         format_completed_quests_suite,
         format_template_quests_suite
     ])

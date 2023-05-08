ce97427ce8878f2204d8f863aedce58db096bdeb
akaClay <domakaclay@gmail.com>
2023-05-08 07:31:22 -0700

    init

---
+++ api/src/spp_extras_api/utils/petsAndMounts.py
@@ -0,0 +1,16 @@
+def format_pet_mount_item_data():
+    all = {'alliance': {}, 'horde': {}, 'neutral': {}}
+
+    return all
+
+
+def format_char_pet_mount_data():
+    all = {}
+
+    return
+
+
+def format_char_skill_data():
+    all = {}
+
+    return

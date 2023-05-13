d528a172c1cea05d278e9b5ba40c435dab55086b
akaClay <domakaclay@gmail.com>
2023-05-13 03:47:22 -0700

    implement format functions

--- api/src/spp_extras_api/utils/pets_mounts.py
+++ api/src/spp_extras_api/utils/pets_mounts.py
@@ -1,3 +1,4 @@
+# Organize pet and mount items by faction and type
 def format_pet_mount_item_data(items):
     all = {
         'alliance': {'pets': {}, 'mounts': {}}, 
@@ -27,13 +28,26 @@ def format_pet_mount_item_data(items):
     return all
 
 
+# Organize known pet and mount spells by character
 def format_char_spell_data(spells):
     all = {}
-    # Sort pet and mount spells by character
-    return
+    for _spell in spells:
+        guid = str(_spell['guid'])
+        spell = _spell['spell']
+        if guid not in all:
+            all[guid] = [spell]
+        else:
+            all[guid].append(spell)
+
+    return all
 
 
+# Organize riding skills by character
 def format_char_skill_data(skills):
     all = {}
-    # Sort riding data skills by char
-    return
+    for skill in skills:
+        guid = str(skill['guid'])
+        value = str(skill['value'])
+        all[guid] = value
+
+    return all

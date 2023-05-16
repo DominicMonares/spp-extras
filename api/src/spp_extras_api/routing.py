b576ee8f4dbd190195e4c79f501f97a18e0d7e35
akaClay <domakaclay@gmail.com>
2023-05-16 05:55:14 -0700

    update file name

--- api/src/spp_extras_api/routing.py
+++ api/src/spp_extras_api/routing.py
@@ -1,11 +1,7 @@
 from django.urls import re_path
-from .consumers.achievements import AccountWideAchievementsConsumer
-from .consumers.pets_mounts import AccountWidePetsMountsConsumer
-from .consumers.reputations import AccountWideReputationsConsumer
+from .consumers.account_wide import AccountWideConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/account_wide/achievements/$', AccountWideAchievementsConsumer.as_asgi()),
-    re_path(r'ws/account_wide/pets_mounts/$', AccountWidePetsMountsConsumer.as_asgi()),
-    re_path(r'ws/account_wide/reputations/$', AccountWideReputationsConsumer.as_asgi())
+    re_path(r'ws/account_wide/transfer/$', AccountWideConsumer.as_asgi()),
 ]
 

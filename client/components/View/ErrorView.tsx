e343af31642067b871d3d6723eefaebf34bed536
akaClay <domakaclay@gmail.com>
2023-05-19 02:17:51 -0700

    add error view styling

--- client/components/View/ErrorView.tsx
+++ client/components/View/ErrorView.tsx
@@ -1,15 +1,20 @@
 import { ViewProps } from "../../types";
 import MainButton from "../MainButton";
 import MainBigHeader from "../MainBigHeader";
+import { useAppSelector } from '../../store/hooks';
 import './View.css';
 
 
 const ErrorView = ({ error, getAllData }: ViewProps) => {
+  const expansion = useAppSelector(state => state.expansion.selected);
+
   return (
-    <div className="error-view">
+    <div>
       <MainBigHeader headerText="Error" />
-      <div className="error-view-body">
+      <div className={`error ${expansion}-error`}>
         <div className="error-text">{error}</div>
+      </div>
+      <div className="error-btn">
         <MainButton handleClick={getAllData} buttonText={'Retry'} />
       </div>
     </div>

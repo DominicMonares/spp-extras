021c74ef3e211d5f2e6cf3fcac89d09d0e81ff05
akaClay <domakaclay@gmail.com>
2023-05-26 03:24:20 -0700

    move client tests Please enter the commit message for your changes. Lines starting

---
+++ tests/renderer/questControlsUtil.test.ts
@@ -0,0 +1,179 @@
+import {
+  characterMenu,
+  classMenu,
+  questTypeMenu,
+  raceMenu,
+  zoneMenu
+} from '../../client/utils';
+import { 
+  Characters,
+  ClassSetting, 
+  Menu, 
+  RaceSetting
+} from '../../client/types';
+import _classMenu from '../../data/menus/classMenu.json';
+import _raceMenu from '../../data//menus/raceMenu.json';
+import _questTypes from '../../data//menus/questTypeMenu.json';
+import _zoneMenu from '../../data//menus/zoneMenu.json';
+import sampleAccounts from '../samples/formattedData/allAccountsChars.json';
+import sampleCharacterMenus from '../samples/client/characterMenus.json';
+import sampleCharacterSettings from '../samples/client/characterSettings.json';
+import sampleClasses from '../samples/client/classes.json';
+import sampleRaces from '../samples/client/races.json';
+
+
+const sampleCharacters = sampleAccounts[0]['characters'] as Characters;
+const { currentCharacterMenu, noCharacterMenu } = sampleCharacterMenus;
+const { orcCharacter } = sampleCharacterSettings;
+const { paladin } = sampleClasses;
+const { orc } = sampleRaces;
+
+describe('characterMenu', () => {
+  it('should create a menu with three characters if no character provided', () => {
+    const result = characterMenu(undefined, sampleCharacters, 'horde');
+    expect(result).toStrictEqual(noCharacterMenu);
+  });
+
+  it('should create a menu with an all characters option and unselected characters', () => {
+    const result = characterMenu(orcCharacter, sampleCharacters, 'horde');
+    expect(result).toStrictEqual(currentCharacterMenu);
+  });
+});
+
+
+describe('classMenu', () => {
+  let menu: Menu, submenu: Menu | undefined;
+  beforeEach(() => {
+    menu = JSON.parse(JSON.stringify(_classMenu));
+    submenu = menu[0]?.submenu;
+  });
+
+  it('should not render the all classes option if no class selected', () => {
+    submenu?.shift();
+    const result = classMenu('wotlk', 'horde', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should not render the shaman option if on classic, alliance, and no class selected', () => {
+    submenu?.splice(0, 2);
+    submenu?.splice(6, 1);
+    const result = classMenu('classic', 'alliance', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should not render the death knight option if not on wotlk and no class selected', () => {
+    submenu?.splice(0, 2);
+    const result = classMenu('tbc', 'horde', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should render all classes option and not render paladin option when class selected', () => {
+    submenu?.splice(5, 1);
+    const result = classMenu('wotlk', 'horde', paladin as ClassSetting);
+    expect(result).toStrictEqual(menu);
+  })
+});
+
+
+describe('raceMenu', () => {
+  let menu: Menu, submenu: Menu | undefined;
+  beforeEach(() => {
+    menu = JSON.parse(JSON.stringify(_raceMenu)).horde;
+    submenu = menu[0]?.submenu;
+  });
+
+  it('should not render the all races option if no race selected', () => {
+    submenu?.shift();
+    const result = raceMenu('wotlk', 'horde', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should not render the blood elf option if on classic and no race selected', () => {
+    submenu?.splice(0, 2);
+    const result = raceMenu('classic', 'horde', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should render all races option and not orc option when race selected', () => {
+    submenu?.splice(2, 1);
+    const result = raceMenu('wotlk', 'horde', orc as RaceSetting);
+    expect(result).toStrictEqual(menu);
+  });
+});
+
+
+describe('questTypeMenu', () => {
+  let menu: Menu, submenu: Menu | undefined;
+  beforeEach(() => {
+    menu = JSON.parse(JSON.stringify(_questTypes));
+    submenu = menu[0]?.submenu;
+  });
+
+  it('should not render the all quest types option if no type selected', () => {
+    submenu?.shift();
+    const result = questTypeMenu('wotlk', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should not render the daily and monthly options if on classic and no type selected', () => {
+    submenu?.shift();
+    submenu?.splice(1, 1);
+    submenu?.pop();
+    const result = questTypeMenu('classic', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should render all types option and not regular option when regular type selected', () => {
+    submenu?.splice(1, 1);
+    const result = questTypeMenu('tbc', 'regular');
+    expect(result).toStrictEqual(menu);
+  });
+});
+
+
+describe('zoneMenu', () => {
+  let menu: Menu, submenu: Menu | undefined;
+  beforeEach(() => {
+    menu = JSON.parse(JSON.stringify(_zoneMenu));
+    submenu = menu[0]?.submenu;
+  });
+
+  it('should not render all zones option if no zone selected', () => {
+    submenu?.shift();
+    const result = zoneMenu('wotlk', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should not render tbc and wotlk zones if on classic and no zone selected', () => {
+    submenu?.shift();
+    submenu?.[0].submenu?.splice(2, 2);
+    submenu?.[1].submenu?.splice(2, 2);
+    submenu?.[2].submenu?.splice(2, 2);
+    submenu?.[3].submenu?.splice(2, 2);
+    submenu?.[0].submenu?.[0].submenu?.splice(13, 2);
+    submenu?.[0].submenu?.[0].submenu?.splice(15, 1);
+    submenu?.[0].submenu?.[0].submenu?.splice(18, 1);
+    submenu?.[0].submenu?.[1].submenu?.splice(2, 2);
+    submenu?.[0].submenu?.[1].submenu?.splice(18, 1);
+    const result = zoneMenu('classic', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should not render tbc if on tbc and no zone selected', () => {
+    submenu?.shift();
+    submenu?.[0].submenu?.splice(3, 1);
+    submenu?.[1].submenu?.splice(3, 1);
+    submenu?.[2].submenu?.splice(3, 1);
+    submenu?.[3].submenu?.splice(3, 1);
+    const result = zoneMenu('tbc', undefined);
+    expect(result).toStrictEqual(menu);
+  });
+
+  it('should render all zones option and not zone if zone selected', () => {
+    if (submenu?.[0].title) submenu[0].submenu = undefined;
+    submenu?.[1].submenu?.[0].submenu?.splice(17, 1);
+    const zone = 'Isle of Quel\'Danas'
+    const result = zoneMenu('wotlk', zone);
+    expect(result).toStrictEqual(menu);
+  });
+});
---
+++ tests/renderer/questViewUtil.test.ts
@@ -0,0 +1,158 @@
+import { createViewQuests, reverseSortViewQuests, sortViewQuests } from '../../client/utils';
+import { QuestTrackerSettings } from '../../client/types';
+import playerQuests from '../samples/client/playerQuests.json';
+import sampleCharacterSettings from '../samples/client/characterSettings.json';
+import sampleClasses from '../samples/client/classes.json';
+import filteredTemplateQuests from '../samples/client/filteredTemplateQuests.json';
+import sampleRaces from '../samples/client/races.json';
+import templateQuests from '../samples/formattedData/templateQuests.json';
+
+
+const { orcCharacter } = sampleCharacterSettings;
+const { paladin, shaman } = sampleClasses;
+const { bloodElf, orc } = sampleRaces;
+
+describe('createViewQuests', () => {
+  it('should return all character marked, filtered template quests', () => {
+    const noCharacterSetting = { faction: 'horde' } as QuestTrackerSettings;
+    const newFilteredQuests = filteredTemplateQuests.slice();
+    newFilteredQuests[1]['completed'] = true;
+    const result = createViewQuests(false, playerQuests, noCharacterSetting, templateQuests);
+    expect(result).toStrictEqual(newFilteredQuests);
+    newFilteredQuests[1]['completed'] = false;
+  });
+
+  it('should return all specific character marked, filtered template quests', () => {
+    const allTypesSetting = { faction: 'horde', character: orcCharacter } as QuestTrackerSettings;
+    const newFilteredQuests = filteredTemplateQuests.slice();
+    newFilteredQuests[2]['completed'] = false;
+    newFilteredQuests[3]['completed'] = false;
+    newFilteredQuests[4]['completed'] = false;
+    const result = createViewQuests(false, playerQuests, allTypesSetting, templateQuests);
+    expect(result).toStrictEqual(newFilteredQuests);
+    newFilteredQuests[2]['completed'] = true;
+    newFilteredQuests[3]['completed'] = true;
+    newFilteredQuests[4]['completed'] = true;
+  });
+
+  it('should return all regular, specific character marked, filtered template quests', () => {
+    const regularTypesSetting = { faction: 'horde', type: 'regular' } as QuestTrackerSettings;
+    const newFilteredQuests = filteredTemplateQuests.slice(0, 5);
+    newFilteredQuests[1]['completed'] = true;
+    const result = createViewQuests(false, playerQuests, regularTypesSetting, templateQuests);
+    expect(result).toStrictEqual(Object.values(newFilteredQuests));
+    newFilteredQuests[1]['completed'] = false;
+  });
+
+  it('should return all monthly, specific character marked, filtered template quests', () => {
+    const monthlyTypesSetting = {
+      faction: 'horde',
+      character: orcCharacter,
+      type: 'monthly'
+    } as QuestTrackerSettings;
+
+    const result = createViewQuests(false, playerQuests, monthlyTypesSetting, templateQuests);
+    expect(result).toStrictEqual(filteredTemplateQuests.slice(5, 6));
+  });
+
+  it('should return all shaman marked, filtered template quests', () => {
+    const shamanSetting = { faction: 'horde', characterClass: shaman } as QuestTrackerSettings;
+    const result = createViewQuests(false, playerQuests, shamanSetting, templateQuests);
+    expect(result).toStrictEqual(filteredTemplateQuests.slice(2, 3));
+  });
+
+  it('should return all blood elf paladin marked, filtered template quests', () => {
+    const raceClassMatchSetting = {
+      faction: 'horde',
+      characterClass: paladin,
+      race: bloodElf
+    } as QuestTrackerSettings;
+
+    const result = createViewQuests(false, playerQuests, raceClassMatchSetting, templateQuests);
+    expect(result).toStrictEqual(filteredTemplateQuests.slice(4, 5));
+  });
+
+  it('should return no quests for race/class mismatch', () => {
+    const raceClassMismatchSetting = {
+      faction: 'horde',
+      characterClass: paladin,
+      race: orc
+    } as QuestTrackerSettings;
+
+    const result = createViewQuests(false, playerQuests, raceClassMismatchSetting, templateQuests);
+    expect(result).toStrictEqual([]);
+  });
+
+  it('should return all classless blood elf marked, filtered template quests', () => {
+    const noClassSetting = { faction: 'horde', race: bloodElf } as QuestTrackerSettings;
+    const result = createViewQuests(false, playerQuests, noClassSetting, templateQuests);
+    expect(result).toStrictEqual(filteredTemplateQuests.slice(3, 5));
+  });
+
+  it('should return all zone marked, filtered template quests', () => {
+    const zoneSetting = { faction: 'horde', zone: 'Ashenvale' } as QuestTrackerSettings;
+    const newFilteredQuests = filteredTemplateQuests.slice(1, 2);
+    newFilteredQuests[0]['completed'] = true;
+    const result = createViewQuests(false, playerQuests, zoneSetting, templateQuests);
+    expect(result).toStrictEqual(newFilteredQuests);
+    newFilteredQuests[0]['completed'] = false;
+  });
+});
+
+
+describe('sortViewQuests', () => {
+  it('should sort in alphabetic order when name selected', () => {
+    const quests = filteredTemplateQuests.slice(0, 4);
+    const expected = [quests[0], quests[0], quests[2], quests[3], quests[1]];
+    expect(sortViewQuests([quests[0], ...quests], 'name')).toStrictEqual(expected);
+  });
+
+  it('should sort in numeric order when id selected', () => {
+    const quests = filteredTemplateQuests.slice(0, 4);
+    const shuffled = [quests[0], quests[2], quests[3], quests[1], quests[0]];
+    const expected = [quests[0], ...quests];
+    expect(sortViewQuests(shuffled, 'id')).toStrictEqual(expected);
+  });
+
+  it('should sort in completed order when status selected', () => {
+    const quests = filteredTemplateQuests.slice(0, 4);
+    quests[0]['completed'] = false;
+    const expected = [quests[2], quests[3], quests[0], quests[1]];
+    expect(sortViewQuests(quests, 'status')).toStrictEqual(expected);
+    expect(sortViewQuests(expected, 'status')).toStrictEqual(expected);
+    expect(sortViewQuests(expected.reverse(), 'status')).toStrictEqual(expected);
+    quests[0]['completed'] = true;
+  });
+
+  it('should return unsorted list when no sort setting provided', () => {
+    expect(sortViewQuests(filteredTemplateQuests, '')).toStrictEqual(filteredTemplateQuests);
+  });
+});
+
+
+describe('reverseSortViewQuests', () => {
+  it('should sort in reverse alphabetic order when name selected', () => {
+    const quests = filteredTemplateQuests.slice(0, 4);
+    const expected = [quests[1], quests[3], quests[2], quests[0], quests[0]];
+    expect(reverseSortViewQuests([...quests, quests[0]], 'name')).toStrictEqual(expected);
+  });
+
+  it('should sort in reverse numeric order when id selected', () => {
+    const quests = filteredTemplateQuests.slice(0, 4);
+    expect(reverseSortViewQuests(quests, 'id')).toStrictEqual(quests.reverse());
+  });
+
+  it('should sort in reverse completed order when status selected', () => {
+    const quests = filteredTemplateQuests.slice(0, 4);
+    quests[0]['completed'] = false;
+    const expected = [quests[1], quests[0], quests[3], quests[2]];
+    expect(reverseSortViewQuests(quests, 'status')).toStrictEqual(expected);
+    expect(reverseSortViewQuests(expected, 'status')).toStrictEqual(expected);
+    expect(reverseSortViewQuests(expected.reverse(), 'status')).toStrictEqual(expected);
+    quests[0]['completed'] = true;
+  });
+
+  it('should return unsorted list when no sort setting provided', () => {
+    expect(reverseSortViewQuests(filteredTemplateQuests, '')).toStrictEqual(filteredTemplateQuests);
+  });
+});

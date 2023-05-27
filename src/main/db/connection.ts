06df13dba9308f2e254b46ed97d040fb8b3f485a
akaClay <domakaclay@gmail.com>
2023-05-26 19:16:37 -0700

    refactor connect and add disconnect

--- src/main/db/connection.ts
+++ src/main/db/connection.ts
@@ -1,15 +1,34 @@
 import mysql from 'mysql2/promise';
 
-const connection = async (xpac: string, db: string) => {
-  const conn = await mysql.createConnection({
+export const connect = async (xpac: string, db: string) => { // TEMP TYPES
+  const settings = {
     host: 'localhost',
     port: 3310,
     user: 'root',
     password: '123456',
     database: `${xpac}${db}`
-  });
+  };
 
-  return conn;
-};
+  try {
+    console.log(`Connecting to ${xpac}${db}...`);
+    const connection = await mysql.createConnection(settings);
+    console.log(`Connected to ${xpac}${db}!`);
+    return connection;
+  } catch (err) {
+    err = `Failed to connect to ${xpac}${db}!\n${err}`;
+    console.error(err);
+    throw err;
+  }
+}
 
-export default connection;
+export const disconnect = async (connection: any, xpac: string, db: string) => { // TEMP ANY
+  try {
+    console.log(`Disconnecting from ${xpac}${db}...`);
+    await connection.end();
+    console.log(`Disconnected from ${xpac}${db}!`);
+  } catch (err) {
+    err = `Failed to disconnect from ${xpac}${db}!\n${err}`;
+    console.error(err);
+    throw err;
+  }
+}

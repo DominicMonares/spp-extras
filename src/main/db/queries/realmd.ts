429b3238bfd85359e364c176ba4b5acd82edbca1
akaClay <domakaclay@gmail.com>
2023-05-26 19:39:49 -0700

    implement error handling

--- src/main/db/queries/realmd.ts
+++ src/main/db/queries/realmd.ts
@@ -1,7 +1,15 @@
 export const selAccts = async (conn: any, bots: boolean) => { // TEMP ANY
-  const [rows] = await conn.execute(`
-    SELECT * FROM account
-    ${!bots ? 'WHERE username NOT LIKE "%RNDBOT%;"' : ''}
-  `);
-  return rows;
+  try {
+    console.log('Fetching account data...');
+    const [rows] = await conn.execute(`
+      SELECT * FROM account
+      ${!bots ? 'WHERE username NOT LIKE "%RNDBOT%;"' : ''}
+    `);
+    console.log('Account data fetched!');
+    return rows;
+  } catch (err) {
+    err = `Failed to fetch account data!\n${err}`;
+    console.error(err);
+    throw err;
+  }
 }

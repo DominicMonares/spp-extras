6c4ed3deb7f02cd54776270c8c333639bba3eeef
akaClay <domakaclay@gmail.com>
2023-05-26 17:38:40 -0700

    remove unused comments

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,10 +1,8 @@
 import { ElectronHandler } from 'main/preload';
 
 declare global {
-  // eslint-disable-next-line no-unused-vars
   interface Window {
     electron: ElectronHandler;
-    // database: DatabaseHandler;
   }
 }
 

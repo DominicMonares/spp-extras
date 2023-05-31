4b8e4c32e1302cbbc057be5013ba60bca02976ab
akaClay <domakaclay@gmail.com>
2023-05-31 05:26:59 -0700

    split account queries

--- src/main/db/realmd.ts
+++ src/main/db/realmd.ts
@@ -4,20 +4,39 @@ import { send } from '../../utils';
 // Accounts
 // ----------------------------------------------------------------
 
-export const selAccts = async (conn: any, bots: boolean, reply?: any) => { // TEMP ANY
+export const selPlayerAccts = async (conn: any, reply?: any) => { // TEMP ANY
   const sql = `
     SELECT id, username FROM account
-    ${!bots ? 'WHERE username NOT LIKE "%RNDBOT%"' : ''}
+    WHERE username NOT LIKE "%RNDBOT%"
   `;
   try {
-    const startMsg = 'Fetching account data...';
+    const startMsg = 'Fetching player account data...';
     send(startMsg, reply);
     const [rows] = await conn.query(sql);
-    const successMsg = 'Account data fetched!';
+    const successMsg = 'Player account data fetched!';
     send(successMsg, reply);
     return rows;
   } catch (err) {
-    const errMsg = `Failed to fetch account data!\n${err}`;
+    const errMsg = `Failed to fetch player account data!\n${err}`;
+    send(errMsg, reply);
+    throw err;
+  }
+}
+
+export const selBotAccts = async (conn: any, reply?: any) => { // TEMP ANY
+  const sql = `
+    SELECT id, username FROM account
+    WHERE username LIKE "%RNDBOT%"
+  `;
+  try {
+    const startMsg = 'Fetching bot account data...';
+    send(startMsg, reply);
+    const [rows] = await conn.query(sql);
+    const successMsg = 'Bot account data fetched!';
+    send(successMsg, reply);
+    return rows;
+  } catch (err) {
+    const errMsg = `Failed to fetch bot account data!\n${err}`;
     send(errMsg, reply);
     throw err;
   }

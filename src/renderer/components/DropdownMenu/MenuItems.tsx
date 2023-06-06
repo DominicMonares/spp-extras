4bc8459777cb87bae8e3d7814d6fcd7ab22a65e7
akaClay <domakaclay@gmail.com>
2023-06-06 01:23:42 -0700

    save dropdown styling progress

--- src/renderer/components/DropdownMenu/MenuItems.tsx
+++ src/renderer/components/DropdownMenu/MenuItems.tsx
@@ -31,10 +31,10 @@ const MenuItems = ({ dropdownType, items, depthLevel }: Props) => {
   const { character, characterClass, race, type, zone } = settings;
 
   // Determine whether a menu/submenu should be rendered or not
-  const [dropdown, setDropdown] = useState<boolean>(true);
+  const [dropdown, setDropdown] = useState<boolean>(false);
 
   // Track nested hovering - keeps parent elements hovering when children are hovering
-  const [subHovering, setSubHovering] = useState<boolean>(true);
+  const [subHovering, setSubHovering] = useState<boolean>(false);
 
   const onMouseEnter = () => {
     setDropdown(true);
